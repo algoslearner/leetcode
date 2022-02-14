a99e1591536f2875f63568574f9ea14811250ef1
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-13 17:00:48 -0800

    Update 01_trapping_rain_water.py

--- companies/fb/leetcode/hard/01_trapping_rain_water.py
+++ companies/fb/leetcode/hard/01_trapping_rain_water.py
@@ -11,26 +11,28 @@ Output: 6
 Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
 '''
 
+# TC : O(N)
+# SC : O(N)
+
 import math
 
-def getTrappedRainWater(arr):
-  # Write your code here
-  leftmax = [0] * len(arr)
-  rightmax = [0] * len(arr)
-  
-  maxvalue = -math.inf
-  for i in range(len(arr)):
-    leftmax[i] = max(maxvalue,arr[i])
-    maxvalue = leftmax[i]
-  
-  maxvalue = -math.inf
-  for i in range(len(arr)-1, -1, -1):
-    rightmax[i] = max(maxvalue,arr[i])
-    maxvalue = rightmax[i]
-  
-  # calculate trapped rain water
-  ans = 0
-  for i in range(len(arr)):
-    ans += min(leftmax[i],rightmax[i]) - arr[i]
-  
-  return ans
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        leftmax = [0] * len(height)
+        rightmax = [0] * len(height)
+        
+        maxi = -math.inf
+        for i in range(len(height)):
+            leftmax[i] = max(maxi,height[i])
+            maxi = leftmax[i]
+        
+        maxi = -math.inf
+        for i in range(len(height)-1,-1,-1):
+            rightmax[i] = max(maxi,height[i])
+            maxi = rightmax[i]
+        
+        diff = 0
+        for i in range(len(height)):
+            diff += min(leftmax[i],rightmax[i]) - height[i]
+        
+        return diff
