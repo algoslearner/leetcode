8781a89082f65120646824bafe43cf496df4366b
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-15 11:30:07 -0800

    Update 01_trapping_rain_water.py

--- companies/fb/leetcode/hard/01_trapping_rain_water.py
+++ companies/fb/leetcode/hard/01_trapping_rain_water.py
@@ -36,3 +36,35 @@ class Solution:
             diff += min(leftmax[i],rightmax[i]) - height[i]
         
         return diff
+
+       ##################################################################################################
+       # TWO POINTERS
+       ##################################################################################################
+       
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        # TC : O(N)
+        # SC : O(1)
+        
+        n = len(height)
+        l, r = 0, n-1
+        l_max = r_max = 0
+        ans = 0
+        while l < r :
+            if height[l] <= height[r] :
+                if height[l] >= l_max :
+                    l_max = height[l]
+                else:
+                    ans += l_max-height[l]
+                l += 1
+            else:
+                if height[r] >= r_max :
+                    r_max = height[r]
+                else:
+                    ans += r_max-height[r]
+                r -= 1
+        return ans
+    
+    
+        
