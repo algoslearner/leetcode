9ed9b3ed875cca99836e1c50a6c61831de538cae
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-16 10:01:09 -0800

    Update 04_lowest_common_ancestor_iii.py

--- companies/fb/leetcode/TREE/easy/04_lowest_common_ancestor_iii.py
+++ companies/fb/leetcode/TREE/easy/04_lowest_common_ancestor_iii.py
@@ -63,3 +63,25 @@ class Solution:
             q = q.parent 
         return q
         
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/discuss/932499/Simple-Python-Solution-with-O(1)-space-complexity
+# TC: O(H)
+# SC : O(1)
+# two pointers
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        p1, p2 = p, q
+        while p1 != p2:
+            p1 = p1.parent if p1.parent else q
+            p2 = p2.parent if p2.parent else p
+            
+        return p1
+   
+'''
+The idea is fairly simple (and the same as finding the convergence point of 2 linked lists). 
+We keep two pointers, p1 and p2. Originally, these pointers point to q and p, respectively. 
+Then we follow their parent pointers until they point to the same node. 
+
+When either of the pointers points to root, we set it to the other original starting node. 
+For example, when p1 points to root (i.e p1.parent is None), assign q to p1.
+'''
