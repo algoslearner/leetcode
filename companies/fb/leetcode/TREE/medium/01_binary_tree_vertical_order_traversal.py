8ee239782bfe8a7e4827bb687d2ed5e94017048f
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-16 10:07:40 -0800

    Create 01_binary_tree_vertical_order_traversal.py

---
+++ companies/fb/leetcode/TREE/medium/01_binary_tree_vertical_order_traversal.py
@@ -0,0 +1,50 @@
+'''
+Given the root of a binary tree, return the vertical order traversal of its nodes' values. (i.e., from top to bottom, column by column).
+
+If two nodes are in the same row and column, the order should be from left to right.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
+Example 2:
+
+
+Input: root = [3,9,8,4,0,1,7]
+Output: [[4],[9],[3,0,1],[8],[7]]
+Example 3:
+
+
+Input: root = [3,9,8,4,0,1,7,null,null,null,2,5]
+Output: [[4],[9,5],[3,0,1],[8,2],[7]]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+'''
+
+# BFS
+# TC = O(N logN)
+# SC : O(N)
+
+from collections import defaultdict
+class Solution:
+    def verticalOrder(self, root: TreeNode) -> List[List[int]]:
+        columnTable = defaultdict(list)
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                columnTable[column].append(node.val)
+                
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
+                        
+        return [columnTable[x] for x in sorted(columnTable.keys())]
