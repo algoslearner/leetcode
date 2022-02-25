d3411931dcc61c3ea5553e7613718ab8b0f48a15
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-25 08:55:43 -0800

    Create 01_split_array.py

---
+++ companies/fb/leetcode/pe/01_split_array.py
@@ -0,0 +1,42 @@
+'''
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+'''
+
+# Time : O(n)
+# Space: O(n)
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+        
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
+        
+        for i in range(len(nums)):
+            nextDP = set()
+            for t in dp:
+                nextDP.add(t+nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        return True if target in dp else False
