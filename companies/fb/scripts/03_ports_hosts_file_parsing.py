22545114cb855673f64602b94b76209593b5d010
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-24 17:47:17 -0800

    Create 03_ports_hosts_file_parsing.py

---
+++ companies/fb/scripts/03_ports_hosts_file_parsing.py
@@ -0,0 +1,5 @@
+'''
+Reading from STDIN and receiving two parameters which was host and ports that I need to reach and print status,
+what if this input is the file with 1tb(here interaction with large inputs & optimization)
+'''
+
