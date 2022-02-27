ddd60bd3cd4d41aa2a36bd135ab173d083591c71
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-26 21:36:03 -0800

    Create 30_shortest_path_in_binary_matrix.py

---
+++ companies/fb/leetcode/medium/30_shortest_path_in_binary_matrix.py
@@ -0,0 +1,95 @@
+'''
+Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+
+A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+All the visited cells of the path are 0.
+All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+The length of a clear path is the number of visited cells of this path.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1],[1,0]]
+Output: 2
+Example 2:
+
+
+Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
+Output: 4
+Example 3:
+
+Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
+Output: -1
+ 
+
+Constraints:
+
+n == grid.length
+n == grid[i].length
+1 <= n <= 100
+grid[i][j] is 0 or 1
+'''
+
+# READ
+# https://leetcode.com/problems/shortest-path-in-binary-matrix/solution/
+
+'''
+directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+define function get_neighbors(row, col):
+neighbors = a container to put the neighbors of (row, col) in
+    for each (row_direction, col_direction) pair in directions:
+        neighbor_row = row + row_direction
+        neighbor_col = col + col_direction
+        if (neighbor_row, neighbor_col) is NOT over the edge of the grid AND is 0:
+            add (neighbor_row, neighbor_col) to neighbors
+    return neighbors
+'''
+
+# BFS : Overwriting input
+# Time : O(N)
+# Space: O(N)
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        max_row = len(grid) - 1
+        max_col = len(grid[0]) - 1
+        directions = [
+            (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        
+        # Helper function to find the neighbors of a given cell.
+        def get_neighbours(row, col):
+            for row_difference, col_difference in directions:
+                new_row = row + row_difference
+                new_col = col + col_difference
+                if not(0 <= new_row <= max_row and 0 <= new_col <= max_col):
+                    continue
+                if grid[new_row][new_col] != 0:
+                    continue
+                yield (new_row, new_col)
+        
+        # Check that the first and last cells are open. 
+        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
+            return -1
+        
+        # Set up the BFS.
+        queue = deque()
+        queue.append((0, 0))
+        grid[0][0] = 1 
+        
+        # Carry out the BFS.
+        while queue:
+            row, col = queue.popleft()
+            distance = grid[row][col]
+            if (row, col) == (max_row, max_col):
+                return distance
+            for neighbour_row, neighbour_col in get_neighbours(row, col):
+                grid[neighbour_row][neighbour_col] = distance + 1
+                queue.append((neighbour_row, neighbour_col))
+        
+        # There was no path.
+        return -1
