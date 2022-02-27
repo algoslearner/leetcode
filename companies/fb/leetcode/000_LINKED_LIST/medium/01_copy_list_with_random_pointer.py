26f09d3751e4de78c2cc3e00cb992046a9fb32f8
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-26 21:27:23 -0800

    Create 01_copy_list_with_random_pointer.py

---
+++ companies/fb/leetcode/000_LINKED_LIST/medium/01_copy_list_with_random_pointer.py
@@ -0,0 +1,70 @@
+'''
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+
+For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+
+Return the head of the copied linked list.
+
+The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+Your code will only be given the head of the original linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Example 2:
+
+
+Input: head = [[1,1],[2,1]]
+Output: [[1,1],[2,1]]
+Example 3:
+
+
+
+Input: head = [[3,null],[3,0],[3,null]]
+Output: [[3,null],[3,0],[3,null]]
+ 
+
+Constraints:
+
+0 <= n <= 1000
+-104 <= Node.val <= 104
+Node.random is null or is pointing to some node in the linked list.
+'''
+
+# https://leetcode.com/problems/copy-list-with-random-pointer/discuss/43491/A-solution-with-constant-space-complexity-O(1)-and-linear-time-complexity-O(N)
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+# we can modify 'random' field instead of the 'next' field in the copied list:
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # create new nodes
+        node = head
+        while node:
+            node.random, node = Node(node.val, node.random, None), node.next
+        # populate random field of the new node
+        node = head
+        while node:
+            node.random.random, node = node.random.next.random if node.random.next else None, node.next 
+        # restore original list and build new list 
+        head_copy, node = head.random if head else None, head
+        while node:
+            node.random.next, node.random, node = node.next.random if node.next else None, node.random.next, node.next
+        return head_copy
