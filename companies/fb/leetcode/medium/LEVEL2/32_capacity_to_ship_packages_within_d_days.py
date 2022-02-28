4e5fa76cf729464963e2370f7c3499a70513c65e
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-02-28 11:20:50 -0800

    Create 32_capacity_to_ship_packages_within_d_days.py

---
+++ companies/fb/leetcode/medium/LEVEL2/32_capacity_to_ship_packages_within_d_days.py
@@ -0,0 +1,90 @@
+'''
+A conveyor belt has packages that must be shipped from one port to another within days days.
+
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+
+ 
+
+Example 1:
+
+Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+Output: 15
+Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+1st day: 1, 2, 3, 4, 5
+2nd day: 6, 7
+3rd day: 8
+4th day: 9
+5th day: 10
+
+Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
+Example 2:
+
+Input: weights = [3,2,2,4,1,4], days = 3
+Output: 6
+Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days like this:
+1st day: 3, 2
+2nd day: 2, 4
+3rd day: 1, 4
+Example 3:
+
+Input: weights = [1,2,3,1,1], days = 4
+Output: 3
+Explanation:
+1st day: 1
+2nd day: 2
+3rd day: 3
+4th day: 1, 1
+ 
+
+Constraints:
+
+1 <= days <= weights.length <= 5 * 104
+1 <= weights[i] <= 500
+'''
+
+'''
+Explanation
+Given the number of bags,
+return the minimum capacity of each bag,
+so that we can put items one by one into all bags.
+
+We binary search the final result.
+The left bound is max(A),
+The right bound is sum(A).
+
+
+More Good Binary Search Problems
+Here are some similar binary search problems.
+Also find more explanations.
+Good luck and have fun.
+
+Minimum Number of Days to Make m Bouquets
+1. Find the Smallest Divisor Given a Threshold
+2. Divide Chocolate
+3. Capacity To Ship Packages In N Days
+4. Koko Eating Bananas
+5. Minimize Max Distance to Gas Station
+6. Split Array Largest Sum
+'''
+
+# code : 
+# https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/discuss/256729/JavaC%2B%2BPython-Binary-Search
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        left, right = max(weights), sum(weights)
+        while left < right:
+            mid = (left + right) // 2
+            need = 1 # days needed
+            cur = 0 # current cargo
+            for w in weights:
+                if cur + w > mid:
+                    need += 1
+                    cur = 0
+                cur += w
+            if need > days: left = mid + 1
+            else: right = mid
+        return left= mid
+        return left
