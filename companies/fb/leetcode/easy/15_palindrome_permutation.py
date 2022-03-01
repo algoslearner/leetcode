d0bb3ff045bcb47afa43873858864ac5cafeb778
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-01 11:02:30 -0800

    Create 15_palindrome_permutation.py

---
+++ companies/fb/leetcode/easy/15_palindrome_permutation.py
@@ -0,0 +1,36 @@
+'''
+Given a string s, return true if a permutation of the string could form a palindrome.
+
+ 
+
+Example 1:
+
+Input: s = "code"
+Output: false
+Example 2:
+
+Input: s = "aab"
+Output: true
+Example 3:
+
+Input: s = "carerac"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 5000
+s consists of only lowercase English letters.
+'''
+
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        
+        permCheck = set()
+        for i in range(len(s)):
+            if s[i] in permCheck:
+                permCheck.remove(s[i])
+            else:
+                permCheck.add(s[i])
+        return len(permCheck) <= 1
+        
