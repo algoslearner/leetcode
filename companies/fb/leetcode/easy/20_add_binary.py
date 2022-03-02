eef235741f6549900e61bba3ed0754d89eb23d95
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-01 21:34:47 -0800

    Create 20_add_binary.py

---
+++ companies/fb/leetcode/easy/20_add_binary.py
@@ -0,0 +1,54 @@
+'''
+Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+ 
+
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+'''
+
+# https://leetcode.com/problems/add-binary/discuss/1679423/Well-Detailed-Explaination-Java-C%2B%2B-Python-oror-Easy-for-mind-to-Accept-it
+lass Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ""
+        i, j, carry = len(a) - 1, len(b) - 1, 0
+        while i >= 0 or j >= 0:
+            sum = carry;
+            if i >= 0 : sum += ord(a[i]) - ord('0') # ord is use to get value of ASCII character
+            if j >= 0 : sum += ord(b[j]) - ord('0')
+            i, j = i - 1, j - 1
+            carry = 1 if sum > 1 else 0;
+            res += str(sum % 2)
+
+        if carry != 0 : res += str(carry);
+        return res[::-1]
+'''
+      ANALYSIS :-
+Time Complexity :- BigO(max(M, N)), M & N is the length of string a, b;
+Space Complexity :- BigO(max(M, N)), which is the size of "res" object
+'''
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        # BIT MANIPULATION
+        # TC: O(N+M)
+        # SC: O(max(N,M))
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
