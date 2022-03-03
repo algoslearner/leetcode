688613d73ad206bff5fd1009f33c739788f078b5
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-03 11:21:00 -0800

    Create 45_max_size_subarray_sum.py

---
+++ companies/fb/leetcode/medium/LEVEL2/45_max_size_subarray_sum.py
@@ -0,0 +1,53 @@
+'''
+325. Maximum Size Subarray Sum Equals k
+
+Given an integer array nums and an integer k, return the maximum length of a subarray that sums to k. If there is not one, return 0 instead.
+
+ 
+
+Example 1:
+
+Input: nums = [1,-1,5,-2,3], k = 3
+Output: 4
+Explanation: The subarray [1, -1, 5, -2] sums to 3 and is the longest.
+Example 2:
+
+Input: nums = [-2,-1,2,1], k = 1
+Output: 2
+Explanation: The subarray [-1, 2] sums to 1 and is the longest.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 105
+-104 <= nums[i] <= 104
+-109 <= k <= 109
+'''
+
+# https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/solution/
+# TC: O(N)
+# SC : O(N)
+
+class Solution:
+    def maxSubArrayLen(self, nums: List[int], k: int) -> int:
+        prefix_sum = longest_subarray = 0
+        indices = {}
+        
+        for i, num in enumerate(nums):
+            prefix_sum += num
+            
+            # Check if all of the numbers seen so far sum to k.
+            if prefix_sum == k:
+                longest_subarray = i + 1
+                
+            # If any subarray seen so far sums to k, then
+            # update the length of the longest_subarray. 
+            if prefix_sum - k in indices:
+                longest_subarray = max(longest_subarray, i - indices[prefix_sum - k])
+                
+            # Only add the current prefix_sum index pair to the 
+            # map if the prefix_sum is not already in the map.
+            if prefix_sum not in indices:
+                indices[prefix_sum] = i
+        
+        return longest_subarray
