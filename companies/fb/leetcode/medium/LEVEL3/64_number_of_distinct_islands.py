e468770d2c9d741b377d0931cd02ffe4bdaa3fdb
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-04 08:21:08 -0800

    Create 64_number_of_distinct_islands.py

---
+++ companies/fb/leetcode/medium/LEVEL3/64_number_of_distinct_islands.py
@@ -0,0 +1,61 @@
+'''
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+An island is considered to be the same as another if and only if one island can be translated (and not rotated or reflected) to equal the other.
+
+Return the number of distinct islands.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]
+Output: 1
+Example 2:
+
+
+Input: grid = [[1,1,0,1,1],[1,0,0,0,0],[0,0,0,0,1],[1,1,0,1,1]]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 50
+grid[i][j] is either 0 or 1.
+'''
+
+# TC: O(N.M)
+# SC: O(N,M)
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+
+        # Do a DFS to find all cells in the current island.
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
+                return
+            if (row, col) in seen or not grid[row][col]:
+                return
+            seen.add((row, col))
+            current_island.add((row - row_origin, col - col_origin))
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+        
+        # Repeatedly start DFS's as long as there are islands remaining.
+        seen = set()
+        unique_islands = set()
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                current_island = set()
+                row_origin = row
+                col_origin = col
+                dfs(row, col)
+                if current_island:
+                    unique_islands.add(frozenset(current_island))
+        
+        return len(unique_islands)
+        
