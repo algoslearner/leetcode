352ac437da04c3eb23d15330b00a5f8aaad53c2f
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-04 15:41:51 -0800

    Create 77_longest_consecutive_sequence.py

---
+++ companies/fb/leetcode/medium/LEVEL3/77_longest_consecutive_sequence.py
@@ -0,0 +1,68 @@
+'''
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+'''
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # SORT, TC:O(n log n), SC: O(1)
+        
+        if not nums:
+            return 0
+
+        nums.sort()
+
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                if nums[i] == nums[i-1]+1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
+        return max(longest_streak, current_streak)
+      
+
+# https://leetcode.com/problems/longest-consecutive-sequence/solution/      
+# HASHSET: TC: O(n), SC: O(n)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # HASHSET, TC:O(n), SC: O(n)
+        
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
