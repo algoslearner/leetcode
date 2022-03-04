106dd37344393a0d7c9ecc660194682a00b957bf
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-04 15:57:53 -0800

    Create 79_partition_labels.py

---
+++ companies/fb/leetcode/medium/LEVEL3/79_partition_labels.py
@@ -0,0 +1,46 @@
+'''
+You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+
+Return a list of integers representing the size of these parts.
+
+ 
+
+Example 1:
+
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+Example 2:
+
+Input: s = "eccbbbbdec"
+Output: [10]
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+'''
+
+# GREEDY
+# TC: O(n)
+# SC: O(1)
+https://leetcode.com/problems/partition-labels/solution/
+  
+  class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last = {c: i for i, c in enumerate(s)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(s):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i - anchor + 1)
+                anchor = i + 1
+            
+        return ans
