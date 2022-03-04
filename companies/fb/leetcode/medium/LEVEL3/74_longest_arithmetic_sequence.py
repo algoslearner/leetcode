52241495142b608bda69a0056a8c03bf1a5c28f1
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-04 14:34:17 -0800

    Create 74_longest_arithmetic_sequence.py

---
+++ companies/fb/leetcode/medium/LEVEL3/74_longest_arithmetic_sequence.py
@@ -0,0 +1,43 @@
+'''
+Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
+
+Recall that a subsequence of an array nums is a list nums[i1], nums[i2], ..., nums[ik] with 0 <= i1 < i2 < ... < ik <= nums.length - 1, and that a sequence seq is arithmetic if seq[i+1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+
+ 
+
+Example 1:
+
+Input: nums = [3,6,9,12]
+Output: 4
+Explanation: 
+The whole array is an arithmetic sequence with steps of length = 3.
+Example 2:
+
+Input: nums = [9,4,7,2,10]
+Output: 3
+Explanation: 
+The longest arithmetic subsequence is [4,7,10].
+Example 3:
+
+Input: nums = [20,1,15,3,10,5,8]
+Output: 4
+Explanation: 
+The longest arithmetic subsequence is [20,15,10,5].
+ 
+
+Constraints:
+
+2 <= nums.length <= 1000
+0 <= nums[i] <= 500
+'''
+
+# https://leetcode.com/problems/longest-arithmetic-subsequence/discuss/274611/JavaC%2B%2BPython-DP
+# DP
+
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        dp = {}
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                dp[j, nums[j] - nums[i]] = dp.get((i, nums[j] - nums[i]), 1) + 1
+        return max(dp.values())
