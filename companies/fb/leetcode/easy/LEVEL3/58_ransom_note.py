a9ff3b0d2e96bfffe4b4bd180451751384575c65
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-07 13:40:57 -0800

    Create 58_ransom_note.py

---
+++ companies/fb/leetcode/easy/LEVEL3/58_ransom_note.py
@@ -0,0 +1,38 @@
+'''
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+'''
+
+# TC: O(n)
+# SC: O(1)
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in ransomNote:
+            if i in magazine:
+                magazine = magazine.replace(i,"",1)
+            else: return False
+        
+        return True
