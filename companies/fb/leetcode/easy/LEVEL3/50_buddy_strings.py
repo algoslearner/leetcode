9c8f01a22a94dcf4372feb35ffc75f7f72c3348f
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2022-03-07 11:29:51 -0800

    Update 50_buddy_strings.py

--- companies/fb/leetcode/easy/LEVEL3/50_buddy_strings.py
+++ companies/fb/leetcode/easy/LEVEL3/50_buddy_strings.py
@@ -29,12 +29,14 @@ Constraints:
 s and goal consist of lowercase letters.
 '''
 
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/92955/Python-4-lines-with-short-explanation
 '''
- solved it following way.
-#1 - if both strings length not matching, return false
-#2 - if both strings are equal, but have any character repeating more than once, its swappable so return true
-#3 - if more than 2 diffs, it won't match by swapping twice, so return false
-#4 - When the diff is exactly two, for it to be swappable, the sorted values of both diffs should match up and return true, else false.
+Explanation
+If A.length() != B.length(): no possible swap
+
+If A == B, we need swap two same characters. Check is duplicated char in A.
+
+In other cases, we find index for A[i] != B[i]. There should be only 2 diffs and it's our one swap.
 '''
 
 class Solution:
@@ -43,21 +45,8 @@ class Solution:
         # TC: O(N)
         # SC : O(1)
         
-        if len(s) != len(goal): 
-            return False
+        if len(s) != len(goal): return False
+        if s == goal and len(set(s)) < len(s): return True
         
-        if s == goal:
-            seen = set()
-            for a in s:
-                if a in seen:
-                    return True
-                seen.add(a)
-            return False
-
-        pairs = []
-        for a, b in zip(s, goal):
-            if a != b:
-                pairs.append((a, b))
-            if len(pairs) >= 3: 
-                return False
-        return len(pairs) == 2 and pairs[0] == pairs[1][::-1]
+        dif = [(a, b) for a, b in zip(s, goal) if a != b]
+        return len(dif) == 2 and dif[0] == dif[1][::-1]
