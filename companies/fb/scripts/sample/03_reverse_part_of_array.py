99e4b80e047812b6a3ecd1edd015aea3045c63c5
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-09 07:28:04 -0800

    Update 03_reverse_part_of_array.py

--- companies/fb/scripts/sample/03_reverse_part_of_array.py
+++ companies/fb/scripts/sample/03_reverse_part_of_array.py
@@ -1,4 +1,9 @@
 '''
 In a given array, rotate a part of the array for which the index is given
+
+or 
+
+Given a vector/ array and 2 numbers, eg 2 and 5. Between those 2 positions in the vector (2 and 5) reverse the order of the elements
+
 '''
 
