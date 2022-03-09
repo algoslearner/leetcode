2bbadd473a3b2e82d3f0a5afa6dfc0edbd6be360
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-09 07:24:49 -0800

    Create 02_split_array.py

---
+++ companies/fb/scripts/sample/02_split_array.py
@@ -0,0 +1,8 @@
+'''
+Given an array of integers greater than zero, f
+ind if it is possible to split it in two (without reordering the elements), 
+such that the sum of the two resulting arrays is the same. 
+
+Print the resulting arrays 
+'''
+
