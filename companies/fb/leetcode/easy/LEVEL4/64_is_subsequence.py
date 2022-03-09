b235dacba1dd5fe08144cdb0136a903cfbc0642a
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-08 16:54:30 -0800

    Create 64_is_subsequence.py

---
+++ companies/fb/leetcode/easy/LEVEL4/64_is_subsequence.py
@@ -0,0 +1,42 @@
+'''
+Given two strings s and t, 
+return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string 
+by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ 
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, 
+and you want to check one by one to see if t has its subsequence. 
+In this scenario, how would you change your code?
+'''
+
+LEFT_BOUND, RIGHT_BOUND = len(s), len(t)
+
+        p_left = p_right = 0
+        while p_left < LEFT_BOUND and p_right < RIGHT_BOUND:
+            # move both pointers or just the right pointer
+            if s[p_left] == t[p_right]:
+                p_left += 1
+            p_right += 1
+
+        return p_left == LEFT_BOUND
