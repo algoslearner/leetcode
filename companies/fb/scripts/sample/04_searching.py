769538e5ae9c23240c47cd4681d10abe3ad21b4b
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-09 07:33:36 -0800

    Create 04_searching.py

---
+++ companies/fb/scripts/sample/04_searching.py
@@ -0,0 +1,17 @@
+'''
+Searching algorithms
+ 1. (*high frequency question) Generate a minesweeper grid (2x3) with 3 randomly-placed mines (solution)
+
+Implement Minesweeper
+Minesweeper is a game where the objective is correctly identify the location of all mines in a given grid. 
+You are given a uniform grid of gray squares in the beginning of the game. 
+Each square contains either a mine (indicated by a value of 9), or an empty square. 
+Empty squares have a number indicating the count of mines in the adjacent squares. 
+Empty squares can have counts from zero (no adjacent mines) up to 8 (all adjacent squares are mines).
+If you were to take a complete grid, for example, you can see which squares have mines and which squares are empty:
+
+
+2. write a function that finds a ship and return its coordinates: (battleship)
+
+3. For a given set of software checkins, write a program that will determine which part along the branch where the fault lies. (solution): (first bad version)
+'''
