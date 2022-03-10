3a792280a88957242bc163dba99e81138898fe5f
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-09 16:17:54 -0800

    Update 05_product_of_array_except_self.py

--- companies/fb/leetcode/medium/05_product_of_array_except_self.py
+++ companies/fb/leetcode/medium/05_product_of_array_except_self.py
@@ -44,3 +44,27 @@ class Solution:
 			
         return output
         
+
+# LEFT and RIGHT PRODUCT LISTS
+# TC: O(n)
+# SC: O(n)
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        length = len(nums)
+        L, R, answer = [0]*length, [0]*length, [0]*length
+        
+        L[0] = 1
+        for i in range(1, length):
+            L[i] = nums[i - 1] * L[i - 1]
+        
+        R[length - 1] = 1
+        for i in reversed(range(length - 1)):
+            R[i] = nums[i + 1] * R[i + 1]
+        
+        # Constructing the answer array
+        for i in range(length):
+            answer[i] = L[i] * R[i]
+        
+        return answer
