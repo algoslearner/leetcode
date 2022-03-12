462d8ada43dfe4b24724fd7fd1a049f2c63da06e
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-12 11:39:43 -0800

    Update 87_longest_word_in_dictionary.py

--- companies/fb/leetcode/medium/LEVEL3/87_longest_word_in_dictionary.py
+++ companies/fb/leetcode/medium/LEVEL3/87_longest_word_in_dictionary.py
@@ -39,6 +39,8 @@ Alternatively, we could have sorted the words beforehand, so that we know the wo
 
 class Solution:
     def longestWord(self, words: List[str]) -> str:
+        # Brute Force: set and sort
+        # TC: O(n2), SC: O(n2)
         wordset = set(words)
         words.sort(key = lambda c: (-len(c), c))
         for word in words:
@@ -46,3 +48,40 @@ class Solution:
                 return word
 
         return ""
+       
+       
+###########################################
+# Build Trie + DFS
+'''
+Intuition
+As prefixes of strings are involved, this is usually a natural fit for a trie (a prefix tree.)
+
+Algorithm
+Put every word in a trie, then depth-first-search from the start of the trie, only searching nodes that ended a word. 
+Every node found (except the root, which is a special case) then represents a word with all it's prefixes present. 
+We take the best such word.
+
+In Python, we showcase a method using defaultdict.
+'''
+# TC: O(sum(n))
+# SC: O(sum(n))
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        Trie = lambda: collections.defaultdict(Trie)
+        trie = Trie()
+        END = True
+
+        for i, word in enumerate(words):
+            reduce(dict.__getitem__, word, trie)[END] = i
+
+        stack = list(trie.values())
+        ans = ""
+        while stack:
+            cur = stack.pop()
+            if END in cur:
+                word = words[cur[END]]
+                if len(word) > len(ans) or len(word) == len(ans) and word < ans:
+                    ans = word
+                stack.extend([cur[letter] for letter in cur if letter != END])
+
+        return ans
