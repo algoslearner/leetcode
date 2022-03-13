e0c194480c73c4b3ed5766ed8aea29cb10777b45
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-13 16:24:45 -0700

    Update 106_palindromic_substring.py

--- companies/fb/leetcode/medium/LEVEL4/106_palindromic_substring.py
+++ companies/fb/leetcode/medium/LEVEL4/106_palindromic_substring.py
@@ -49,3 +49,25 @@ class Solution:
 ############################################################################
 # For optimizations READ: 
 # https://leetcode.com/problems/palindromic-substrings/solution/
+
+# EXPAND FROM CENTER
+# TC: O(N^2)
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+        global result
+        result = 0
+        
+        def expandFromCenter(i, j):
+            global result
+            while 0 <= i < len(s) and 0 <= j < len(s) and s[i] == s[j]:                
+                result += 1
+                i -= 1
+                j += 1        
+        
+        for idx in range(len(s)):
+            expandFromCenter(idx, idx) # odd expansion
+            expandFromCenter(idx, idx+1) # even expansion
+        
+        return result
