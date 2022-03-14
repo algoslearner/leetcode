c96bc2f17f73efd9cd05f2ac4a46efcf6a19fcc6
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-13 17:01:27 -0700

    Create 110_valid_parenthesis.py

---
+++ companies/fb/leetcode/medium/LEVEL4/110_valid_parenthesis.py
@@ -0,0 +1,44 @@
+'''
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'.
+'''
+# GREEDY
+# TC: O(N)
+# SC: O(1)
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        lo = hi = 0
+        for c in s:
+            lo += 1 if c == '(' else -1
+            hi += 1 if c != ')' else -1
+            if hi < 0: break
+            lo = max(lo, 0)
+
+        return lo == 0
