4c091846e966d71bf69f7344ae00cbdfba2f88f5
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-16 16:29:48 -0700

    Create 09_pair_sum.py

---
+++ companies/fb/scripts/sample/09_pair_sum.py
@@ -0,0 +1,8 @@
+'''
+Given 2 arrays of cost of departure and return [1,4,6,7,8] and [2,5,10,8,6] find the min cost of round travel. 
+Same day round travel can be possible. Here answer would be 3
+
+Was able to solve both, bust wasted lot of time on first. Wasnt able to give optimised solution for second. Result - Reject
+'''
+
+Similar to valid parenthesis but balance the string and return
