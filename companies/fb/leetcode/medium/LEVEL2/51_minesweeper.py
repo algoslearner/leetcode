dbd86bf80db23cc32c041b16ca39970669c3de31
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-16 16:13:03 -0700

    Update 51_minesweeper.py

--- companies/fb/leetcode/medium/LEVEL2/51_minesweeper.py
+++ companies/fb/leetcode/medium/LEVEL2/51_minesweeper.py
@@ -42,6 +42,8 @@ click.length == 2
 board[clickr][clickc] is either 'M' or 'E'.
 '''
 
+##########################################################################
+# one way
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         # DFS
@@ -57,3 +59,61 @@ class Solution:
                     for r, c in directions:
                         self.updateBoard(board, [row + r, col + c])
         return board
+
+       
+#########################################################################
+# below implementation is easier to understand
+##################################
+# https://leetcode.com/problems/minesweeper/discuss/99826/Java-Solution-DFS-%2B-BFS
+'''
+This is a typical Search problem, either by using DFS or BFS. Search rules:
+
+If click on a mine ('M'), mark it as 'X', stop further search.
+If click on an empty cell ('E'), depends on how many surrounding mine:
+2.1 Has surrounding mine(s), mark it with number of surrounding mine(s), stop further search.
+2.2 No surrounding mine, mark it as 'B', continue search its 8 neighbors.
+'''
+
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        i = click[0]
+        j = click[1]
+        # found mine
+        if board[i][j] == 'M':
+            board[i][j] = 'X'
+        else:
+            self.updateOneSquare(board, i , j)
+        return board
+    
+    def updateOneSquare(self,board: List[List[str]], i: int, j: int ) -> None:
+        # check for invalid cases
+        if (i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != 'E'):
+            return
+        
+        nearby_mines_num = self.findNearbyMinesNumber(board, i, j)
+        if nearby_mines_num > 0:
+            board[i][j] = str(nearby_mines_num)
+        else:
+            board[i][j] = 'B'
+            self.updateOneSquare(board, i - 1, j - 1)
+            self.updateOneSquare(board, i - 1, j)
+            self.updateOneSquare(board, i - 1, j + 1)
+            self.updateOneSquare(board, i, j - 1)
+            self.updateOneSquare(board, i, j + 1)
+            self.updateOneSquare(board, i + 1, j - 1)
+            self.updateOneSquare(board, i + 1, j)
+            self.updateOneSquare(board, i + 1, j + 1)
+            
+    def findNearbyMinesNumber(self, board: List[List[str]], i: int, j: int) -> int:
+        nearby_mines = self.hasMine(board, i - 1, j - 1) + self.hasMine(board, i - 1, j) + self.hasMine(board, i - 1, j + 1) + self.hasMine(board, i, j - 1) + self.hasMine(board, i, j + 1) + self.hasMine(board, i + 1, j - 1) + self.hasMine(board, i + 1, j) + self.hasMine(board, i + 1, j + 1)
+        return nearby_mines
+            
+    def hasMine(self, board: List[List[str]], i: int, j: int) -> int:
+        # invalid cases
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+            return 0
+        
+        if board[i][j] == 'M':
+            return 1
+        else:
+            return 0
