77f08f8837bef2a02b6cf3b6d781dc40ff8cea1e
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-17 10:57:41 -0700

    Update 01_facebook_label.py

--- companies/fb/scripts/sample/01_facebook_label.py
+++ companies/fb/scripts/sample/01_facebook_label.py
@@ -1,11 +1,9 @@
 '''
 Given a word, find out how many times you'd need the word "facebook" to spell it out.
 '''
+import collections
 
-def countLabels(word: str) -> int:
-  
-  if set(word) != set("facebook"):
-    return -1
+def countLabels(word):
   
   # word freq count
   wordfreq = collections.Counter(word)
@@ -13,6 +11,29 @@ def countLabels(word: str) -> int:
   
   count = 1
   for c in wordfreq.keys():
-    if wordfreq[c] > count * fbfreq[c]:
-      count += 1
-  return count
+    if c not in fbfreq:
+      print("Error: given word has extra alphabets")
+      return -1
+    elif wordfreq[c] > count * fbfreq[c]: count += 1
+  print(count)
+  
+#########################################################################################  
+# TESTS
+# word = "face"
+# word = "facef"
+# word = "foooce"
+word = "price"
+countLabels(word)
+
+# output: word = "face"
+'''
+1
+'''
+# output: word = "foooce"
+'''
+2
+'''
+#output : word = "price"
+'''
+Error: given word has extra alphabets
+'''
