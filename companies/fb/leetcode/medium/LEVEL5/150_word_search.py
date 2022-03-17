9d99d9faad8ce3b45c9ac8d15362a787813e0f59
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-16 22:00:17 -0700

    Create 150_word_search.py

---
+++ companies/fb/leetcode/medium/LEVEL5/150_word_search.py
@@ -0,0 +1,113 @@
+'''
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+ 
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+ 
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
+'''
+
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+        self.board = board
+
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.backtrack(row, col, word):
+                    return True
+
+        # no match found after all exploration
+        return False
+
+
+    def backtrack(self, row, col, suffix):
+        # bottom case: we find match for each letter in the word
+        if len(suffix) == 0:
+            return True
+
+        # Check the current status, before jumping into backtracking
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
+                or self.board[row][col] != suffix[0]:
+            return False
+
+        ret = False
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            ret = self.backtrack(row + rowOffset, col + colOffset, suffix[1:])
+            # break instead of return directly to do some cleanup afterwards
+            if ret: break
+
+        # revert the change, a clean slate and no side-effect
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, and did not find any match
+        return ret
+
+        '''
+    def backtrack(self, row, col, suffix):
+        """
+            backtracking with side-effect,
+               the matched letter in the board would be marked with "#".
+        """
+        # bottom case: we find match for each letter in the word
+        if len(suffix) == 0:
+            return True
+
+        # Check the current status, before jumping into backtracking
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
+                or self.board[row][col] != suffix[0]:
+            return False
+
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
+            # sudden-death return, no cleanup.
+            if self.backtrack(row + rowOffset, col + colOffset, suffix[1:]):
+                return True
+
+        # revert the marking
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, and did not find any match
+        return False
+        '''
+        
+  # TC: O(N. 3^L)
+  # SC: O(L), length of a word
