1d2a80948743721f44211ebde88bb7fd344ee7b3
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-17 15:57:42 -0700

    Update 05_SEARCHING_battleship.py

--- companies/fb/scripts/sample/05_SEARCHING_battleship.py
+++ companies/fb/scripts/sample/05_SEARCHING_battleship.py
@@ -1,5 +1,9 @@
 '''
 write a function that finds a ship and return its coordinates: (battleship)
+
+Algorithm phone interview: if you had a program to play the Battleship game, 
+and you had the method "fireBomb()" that took coords on a square matrix of any size, 
+implement an algorithm that has to use the "fireBomb()" method the least amount of times to destroy a battleship.
 '''
 
 
