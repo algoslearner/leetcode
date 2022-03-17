c679f38ca613ac4767c150dd61f26ba372046def
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-17 10:39:46 -0700

    Update 03_reverse_part_of_array.py

--- companies/fb/scripts/sample/03_reverse_part_of_array.py
+++ companies/fb/scripts/sample/03_reverse_part_of_array.py
@@ -7,3 +7,27 @@ Given a vector/ array and 2 numbers, eg 2 and 5. Between those 2 positions in th
 
 '''
 
+def reversePartOfArray(nums, left, right):
+	print(nums)
+	while(left < right):
+		tmp = nums[left]
+		nums[left] = nums[right]
+		nums[right] = tmp
+		left += 1
+		right -= 1
+	print(nums)
+
+# TC: O(n)
+# SC: O(1)
+
+# tests
+nums = [1,2,3,4, 5, 6, 7]
+reversePartOfArray(nums, 2, 5)
+
+# output
+'''
+[1, 2, 3, 4, 5, 6, 7]
+[1, 2, 6, 5, 4, 3, 7]
+'''
+
+
