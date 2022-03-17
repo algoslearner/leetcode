852cd2ee2fde83aab1c4fd64c8aa1c17de800138
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-17 11:15:38 -0700

    Create 00_valid_palindrome.py

---
+++ companies/fb/scripts/sample/00_valid_palindrome.py
@@ -0,0 +1,72 @@
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+'''
+
+###################################################################
+# using in-build reverse
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        #corner case: return false if string is all space
+        if(s.isspace()):
+            return True
+        
+        word = ''
+        for c in s:
+            if (c.isalnum()):
+                word += c
+        
+        word = word.lower()
+        return word == word[::-1]
+        
+        
+        
+######################################################################
+# Two pointers
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            else:
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+                
+        return True
+        
