b1031012dd614da198614687bab11ae7cfeb6fb6
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-21 09:59:51 -0700

    Update 46_longest_palindromic_substring.py

--- companies/fb/leetcode/medium/LEVEL2/46_longest_palindromic_substring.py
+++ companies/fb/leetcode/medium/LEVEL2/46_longest_palindromic_substring.py
@@ -20,6 +20,7 @@ Constraints:
 s consist of only digits and English letters.
 '''
 
+########################################################################################################################
 # Brute force
 # TC: O(n^3)
 # SC: O(1)
@@ -40,4 +41,63 @@ class Solution:
         return output
         
 
+ #########################################################################################################################
  # OPTIMIZED : expand from center     
+ # TC: O(n^2)
+ # SC: O(1)
+ 
+ class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+        def palindromeIndex(l, r):
+            while l >= 0 and r < len(s):
+                if s[l] != s[r]:
+                    break
+                r += 1
+                l -= 1
+            return l+1,r
+        
+        start, end = 0, 0
+        maxlen = 0
+        for i in range(len(s)):
+            # even palindrome
+            l, r = palindromeIndex(i,i)
+            if r -l > maxlen:
+                maxlen = r - l
+                start = l
+                end = r
+                
+            # odd palindrome
+            l, r = palindromeIndex(i, i+1)
+            if r - l > maxlen:
+                maxlen = r- l
+                start = l
+                end = r
+        
+        return s[start:end]
+            
+
+###########################################################################################################################
+# another cleaner logic
+# TC: O(n^2)
+# SC: O(1)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        return s == s[::-1]
+    
+    def longestPalindrome(self, s):
+        res = ""
+        for i in range(len(s)):        
+            odd  = self.palindromeAt(s, i, i)
+            even = self.palindromeAt(s, i, i+1)
+        
+            res = max(res, odd, even, key=len)
+        return res
+ 
+    # starting at l,r expand outwards to find the biggest palindrome
+    def palindromeAt(self, s, l, r):    
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
