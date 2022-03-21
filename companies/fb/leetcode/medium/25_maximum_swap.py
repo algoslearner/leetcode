6a774b7c5fb2c6e70b3b30e5b4973558832877a2
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-21 16:58:04 -0700

    Update 25_maximum_swap.py

--- companies/fb/leetcode/medium/25_maximum_swap.py
+++ companies/fb/leetcode/medium/25_maximum_swap.py
@@ -21,26 +21,28 @@ Constraints:
 
 0 <= num <= 108
 
-
-Basic idea:
-Find a index i, where there is a increasing order
-On the right side of i, find the max value (max_val) and its index (max_idx)
-On the left side of i, find the most left value and its index (left_idx), which is less than max_val
-Swap above left_idx and max_idx if necessary
 '''
+
+
+############################################################################
+# Iterate the list of digits backwards and keep tracking of the max digit, as well as the digit that is smaller than the max.
+# TC: O(n)
+# SC: O(1)
 
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        s = list(str(num))
-        n = len(s)
-        for i in range(n-1):                                # find index where s[i] < s[i+1], meaning a chance to flip
-            if s[i] < s[i+1]: break
-        else: return num                                    # if nothing find, return num
-        max_idx, max_val = i+1, s[i+1]                      # keep going right, find the maximum value index
-        for j in range(i+1, n):
-            if max_val <= s[j]: max_idx, max_val = j, s[j]
-        left_idx = i                                        # going right from i, find most left value that is less than max_val
-        for j in range(i, -1, -1):    
-            if s[j] < max_val: left_idx = j
-        s[max_idx], s[left_idx] = s[left_idx], s[max_idx]   # swap maximum after i and most left less than max
-        return int(''.join(s))                              # re-create the integer
+        digits = list(str(num))
+        p, q, max_i = -1, -1, len(digits)-1
+        
+        for i in range(max_i-1, -1, -1):
+            if digits[i] > digits[max_i]:
+                max_i = i
+            elif digits[i] < digits[max_i]:
+                p, q = i, max_i
+
+        # swap
+        if p > -1:
+            digits[p], digits[q] = digits[q], digits[p]
+            
+        return int(''.join(digits))
+                
