c1cf2a34f36c651f79a44e78c6a3f820788a8c00
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-21 19:50:41 -0700

    Update 19_next_permutation.py

--- companies/fb/leetcode/medium/19_next_permutation.py
+++ companies/fb/leetcode/medium/19_next_permutation.py
@@ -33,6 +33,41 @@ Constraints:
 0 <= nums[i] <= 100
 '''
 
+#####################################################################################################################
+#
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        N = len(nums)
+        
+        # find pivot
+        pivot = 0
+        for i in range(N-1,0,-1):
+            if nums[i-1] < nums[i]:
+                pivot = i
+                break
+            
+        if pivot == 0:
+            nums.sort()
+            return
+        
+        # then find the 'swap' number from the right, which is the first number > pivot
+        swap = N-1
+        while nums[pivot-1] >= nums[swap]:
+            swap -= 1
+        
+        # swap 
+        nums[swap], nums[pivot-1] = nums[pivot-1], nums[swap]
+        
+        #sort
+        nums[pivot:] = sorted(nums[pivot:])
+            
+
+
+######################################################################################################################
 # TC : O(n)
 # SC : O(1)
 
