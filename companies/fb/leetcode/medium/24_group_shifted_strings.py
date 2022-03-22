8c7ba41b165ef7c85b3bbb40150dc6966e646b6b
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-21 17:12:28 -0700

    Update 24_group_shifted_strings.py

--- companies/fb/leetcode/medium/24_group_shifted_strings.py
+++ companies/fb/leetcode/medium/24_group_shifted_strings.py
@@ -26,38 +26,23 @@ Constraints:
 strings[i] consists of lowercase English letters.
 '''
 
+########################################################################
+# TC: O(Nk)
+# SC: O(Nk)
+
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
         
-        # Hashing
-        # Time : O(Nk)
-        # Space : O(Nk)
-        
-        '''
-        # Create a hash value
-        def get_hash(string: str):
-            key = []
-            for a, b in zip(string, string[1:]):
-                key.append(chr((ord(b) - ord(a)) % 26 + ord('a')))
-            return ''.join(key)
-        
-        # Create a hash value (hash_key) for each string and append the string
-        # to the list of hash values i.e. mapHashToList["cd"] = ["acf", "gil", "xzc"]
-        groups = collections.defaultdict(list)
-        for string in strings:
-            hash_key = get_hash(string)
-            groups[hash_key].append(string)
-        
-        # Return a list of all of the grouped strings
-        return list(groups.values())
-        '''
-        
-        # use the differences between each characters as the key in the map
-        seqmap = defaultdict(list)
         for s in strings:
-            seq = [(ord(s[n+1])-ord(s[n])) % 26 for n in range(len(s)-1)]
-            seqmap[tuple(seq)].append(s)
-
-        return list(seqmap.values())
+            # use shifts between characters as key
+            shifts = []
+            for n in range(len(s)-1):
+                diff = (ord(s[n+1]) - ord(s[n])) % 26
+                shifts.append(diff)
+            hashmap[tuple(shifts)].append(s)
+        
+        return hashmap.values()
+        
         
         
