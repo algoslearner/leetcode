ddf6d6bdeeae0a193f3589cfbecaf20ec2855437
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-23 09:29:28 -0700

    Update and rename 08_count_words.py to 08_count_words_file_parsing.py

--- companies/fb/scripts/08_count_words.py
+++
@@ -1,5 +0,0 @@
-'''
-given stdin arguments as $filepath and $count, print top 10 frequent words in the file.
-
-here $count = 10
-'''
---
+++ companies/fb/scripts/08_count_words_file_parsing.py
@@ -0,0 +1,8 @@
+'''
+Given STDIN arguments as $filepath and $count, print top 10 frequent words in the file. 
+File content is a group of paragraphs, and word separators include spaces, dots, commas, semicolons, exclamations etc.
+
+here $count = 10
+$filepath = /log/filename.txt
+
+'''
