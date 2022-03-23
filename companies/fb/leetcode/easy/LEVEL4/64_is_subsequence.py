4cf8ad2f2bf7063fbc97d50e62e5d34c8142fff8
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-23 16:16:50 -0700

    Update 64_is_subsequence.py

--- companies/fb/leetcode/easy/LEVEL4/64_is_subsequence.py
+++ companies/fb/leetcode/easy/LEVEL4/64_is_subsequence.py
@@ -29,14 +29,3 @@ Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 
 and you want to check one by one to see if t has its subsequence. 
 In this scenario, how would you change your code?
 '''
-
-LEFT_BOUND, RIGHT_BOUND = len(s), len(t)
-
-        p_left = p_right = 0
-        while p_left < LEFT_BOUND and p_right < RIGHT_BOUND:
-            # move both pointers or just the right pointer
-            if s[p_left] == t[p_right]:
-                p_left += 1
-            p_right += 1
-
-        return p_left == LEFT_BOUND
