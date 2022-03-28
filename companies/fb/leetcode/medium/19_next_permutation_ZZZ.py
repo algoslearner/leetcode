c70dc8aae2d679a8e17bf0c7136087405ccdc760
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-28 16:44:59 -0700

    Update 19_next_permutation_ZZZ.py

--- companies/fb/leetcode/medium/19_next_permutation_ZZZ.py
+++ companies/fb/leetcode/medium/19_next_permutation_ZZZ.py
@@ -34,7 +34,7 @@ Constraints:
 '''
 
 #####################################################################################################################
-#
+# TC: O(n), SC: O(1)
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
@@ -76,22 +76,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #>right
-        i = len(nums)-1
-        while i-1>=0 and nums[i-1]>=nums[i]:
-            i -=1
+        # right
+        i = len(nums) - 1
+        while i - 1 >= 0 and nums[i-1] >= nums[i]:
+            i -= 1
         
-        #>left
-        if i-1>=0:
+        # left
+        if i-1 >= 0:
             j = i
-            while j<len(nums) and nums[j]>nums[i-1]:
-                j +=1
+            while j < len(nums) and nums[j] > nums[i-1]:
+                j += 1
             #swap the min-max number
-            nums[i-1],nums[j-1] = nums[j-1],nums[i-1]
+            nums[i-1], nums[j-1] = nums[j-1], nums[i-1]
+        
         m = i
-        n = len(nums)-1
+        n = len(nums) - 1
         while m < n:
-            nums[m],nums[n] = nums[n],nums[m]
-            m +=1
-            n -=1
+            nums[m], nums[n] = nums[n], nums[m]
+            m += 1
+            n -= 1
         
