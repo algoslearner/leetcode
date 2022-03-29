a21fa2725ca8b2755e9b947eb2ccfe9f359c7759
ann <88410184+ann-mich@users.noreply.github.com>
2022-03-28 18:05:12 -0700

    Update 19_next_permutation_ZZZ.py

--- companies/fb/leetcode/medium/19_next_permutation_ZZZ.py
+++ companies/fb/leetcode/medium/19_next_permutation_ZZZ.py
@@ -34,35 +34,33 @@ Constraints:
 '''
 
 #####################################################################################################################
-# TC: O(n), SC: O(1)
+# TC: O(n + nlogn), SC: O(1)
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        N = len(nums)
-        
         # find pivot
         pivot = 0
-        for i in range(N-1,0,-1):
+        for i in range(len(nums)-1,0,-1):
             if nums[i-1] < nums[i]:
                 pivot = i
                 break
-            
+        
         if pivot == 0:
             nums.sort()
             return
         
-        # then find the 'swap' number from the right, which is the first number > pivot
-        swap = N-1
+        # find swap number, which is first number from right > nums[pivot-1]
+        swap = len(nums) - 1
         while nums[pivot-1] >= nums[swap]:
             swap -= 1
         
-        # swap 
-        nums[swap], nums[pivot-1] = nums[pivot-1], nums[swap]
+        # swap
+        nums[pivot-1], nums[swap] = nums[swap], nums[pivot-1]
         
-        #sort
+        # sort
         nums[pivot:] = sorted(nums[pivot:])
             
 
