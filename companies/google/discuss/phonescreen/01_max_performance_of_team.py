5e86dd922a0db134c1948696c184c053687ec758
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-06 14:52:08 -0700

    Update 01_max_performance_of_team.py

--- companies/google/discuss/phonescreen/01_max_performance_of_team.py
+++ companies/google/discuss/phonescreen/01_max_performance_of_team.py
@@ -1,28 +1,4 @@
-#GOOGLE QUESTION
-'''
-You have n items, cost is c(i) and delivery cost is d(i). 
-If customer orders more than one item, then they get it for the minimum delivery cost. 
-How do you find the maximum amount of money you can make after delivering m items? 
-
-I know this is a knapsack problem, but I just can't find a way to solve it. 
-And I want to make sure I am able to solve these problems for future interviews.
-
-For e.g.
-cost and delivery cost respectively (first column being cost, second being delivery cost):
-Item 1: 7, 10
-Item 2: 4, 15
-Item3: 8, 1
-
-m = 2
-Input format (java): int n, int m, int[][] arr where n is the total number of items, 
-m is the maximum number of items you can deliver, 
-arr has each row with first element being cost, second being delivery cost.
-
-Output: 31 (You choose the first two items because if you chose the 3rd item, 
-the delivery cost for 2 items would be 1 + 1 (since 1 is the mimimum delivery cost) so you would end up with 23 + 2 = 25.
-'''
-
-# RELATED LEETCODE QUESTION
+# RELATED LEETCODE QUESTION : 1383. Maximum Performance of a Team
 '''
 You are given two integers n and k and two integer arrays speed and efficiency both of length n. There are n engineers numbered from 1 to n. speed[i] and efficiency[i] represent the speed and efficiency of the ith engineer respectively.
 
@@ -87,3 +63,61 @@ class Solution:
         # to prevent overflow of output
         modulo = 10 ** 9 + 7    
         return perf % modulo
+
+       
+ ####################################################################################################
+ #GOOGLE QUESTION
+'''
+You have n items, cost is c(i) and delivery cost is d(i). 
+If customer orders more than one item, then they get it for the minimum delivery cost. 
+How do you find the maximum amount of money you can make after delivering m items? 
+
+I know this is a knapsack problem, but I just can't find a way to solve it. 
+And I want to make sure I am able to solve these problems for future interviews.
+
+For e.g.
+cost and delivery cost respectively (first column being cost, second being delivery cost):
+Item 1: 7, 10
+Item 2: 4, 15
+Item3: 8, 1
+
+m = 2
+Input format (java): int n, int m, int[][] arr where n is the total number of items, 
+m is the maximum number of items you can deliver, 
+arr has each row with first element being cost, second being delivery cost.
+
+Output: 31 (You choose the first two items because if you chose the 3rd item, 
+the delivery cost for 2 items would be 1 + 1 (since 1 is the mimimum delivery cost) so you would end up with 23 + 2 = 25.
+'''
+
+####################################################################################################
+# https://leetcode.com/discuss/interview-question/889942/any-idea-how-to-solve-this-problem-i-just-bombed-it-in-an-interview
+# My solution with priority_queue in Python
+
+import heapq
+def solve(n, cost, delivery, m):
+    orders = sorted(zip(delivery, cost), reverse=True)
+
+    pq = []
+    minRate = float('inf')
+    sumCost = 0
+    res = 0
+    for i in range(n):
+        d, c = orders[i]
+        heapq.heappush(pq, (d, c))
+        minRate = min(minRate, d)
+        sumCost += c
+        if len(pq) > m:
+            minRate, mc = heapq.heappop(pq)
+            sumCost -= mc
+        res = max(res, (minRate*len(pq)) + sumCost)
+    print(res) # prints 31 with provided input
+
+'''
+n = 3
+cost = [7,4,8]
+delivery = [10,15,1]
+m = 2
+solve(n, cost, delivery, m)
+'''
+####################################################################################################
