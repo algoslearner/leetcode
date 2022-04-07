b598d9e88546874bc21608bc885fa35344179e53
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-07 15:25:52 -0700

    Create 01_teamblind.py

---
+++ companies/google/discuss/onsite/01_teamblind.py
@@ -0,0 +1,10 @@
+'''
+I’ve skipped phone interview and had onsite. Here Is my questions.
+
+* []Take 2 strings return index of char where is not the same
+* []Design jsbin
+* []Given a function return a function that executed original function 1 second later
+* []Given a timeline write the JavaScript to select all nodes within selection of timeline
+* []Given a 2 dimensional array add the the diagonals
+* []How would you create google analytics script
+'''
