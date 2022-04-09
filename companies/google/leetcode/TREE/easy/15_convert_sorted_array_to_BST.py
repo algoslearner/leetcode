1cd248f07f7146e20a7ef6c663464698a21a42a4
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-09 06:04:58 -0700

    Create 15_convert_sorted_array_to_BST.py

---
+++ companies/google/leetcode/TREE/easy/15_convert_sorted_array_to_BST.py
@@ -0,0 +1,57 @@
+'''
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.
+'''
+
+#######################################################################################################
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+#######################################################################################################  
+# TC: O(N)
+# SC: O(log N), since tree is height balanced. Also, O(N) space used to store the output is not included in SC
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def helper(left, right):
+            if left > right:
+                return None
+            
+            # always choose left middle node as root
+            p = (left + right) // 2
+            
+            # preorder traversal: node, left, right
+            root = TreeNode(nums[p])
+            root.left = helper(left, p - 1)
+            root.right = helper(p + 1, right)
+            return root
+        
+        return helper(0, len(nums) - 1)
