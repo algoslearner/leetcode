b87f02c298390d11eb47ecd140aeeecd1cfe53d3
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-12 11:12:42 -0700

    Create 06_game_of_life.py

---
+++ companies/google/leetcode/medium/06_game_of_life.py
@@ -0,0 +1,156 @@
+'''
+According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+The board is made up of an m x n grid of cells, where each cell has an initial state: live (represented by a 1) or dead (represented by a 0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+Any live cell with fewer than two live neighbors dies as if caused by under-population.
+Any live cell with two or three live neighbors lives on to the next generation.
+Any live cell with more than three live neighbors dies, as if by over-population.
+Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously. Given the current state of the m x n grid board, return the next state.
+
+ 
+
+Example 1:
+
+
+Input: board = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]]
+Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+Example 2:
+
+
+Input: board = [[1,1],[1,0]]
+Output: [[1,1],[1,1]]
+ 
+
+Constraints:
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 25
+board[i][j] is 0 or 1.
+ 
+
+Follow up:
+
+Could you solve it in-place? Remember that the board needs to be updated simultaneously: You cannot update some cells first and then use their updated values to update other cells.
+In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches upon the border of the array (i.e., live cells reach the border). How would you address these problems?
+'''
+
+######################################################################################################
+# he only trick is to denote live --> die as -1 and die --> live as 2...... Anyway that is a good idea
+# Brute force
+# TC: O(mn)
+# SC: O(mn)
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        # Neighbors array to find 8 neighboring cells for a given cell
+        neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+        rows = len(board)
+        cols = len(board[0])
+
+        # Create a copy of the original board
+        copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
+
+        # Iterate through board cell by cell.
+        for row in range(rows):
+            for col in range(cols):
+
+                # For each cell count the number of live neighbors.
+                live_neighbors = 0
+                for neighbor in neighbors:
+
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    # Check the validity of the neighboring cell and if it was originally a live cell.
+                    # The evaluation is done against the copy, since that is never updated.
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                        live_neighbors += 1
+
+                # Rule 1 or Rule 3        
+                if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[row][col] = 0
+                # Rule 4
+                if copy_board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] = 1
+                    
+                    
+                    
+######################################################################################################
+# Optimized : O(1) space
+# TC: O(mn)
+# SC: O(1)
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        # Neighbors array to find 8 neighboring cells for a given cell
+        neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+        rows = len(board)
+        cols = len(board[0])
+
+        # Iterate through board cell by cell.
+        for row in range(rows):
+            for col in range(cols):
+
+                # For each cell count the number of live neighbors.
+                live_neighbors = 0
+                for neighbor in neighbors:
+
+                    # row and column of the neighboring cell
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    # Check the validity of the neighboring cell and if it was originally a live cell.
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and abs(board[r][c]) == 1:
+                        live_neighbors += 1
+
+                # Rule 1 or Rule 3
+                if board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    # -1 signifies the cell is now dead but originally was live.
+                    board[row][col] = -1
+                # Rule 4
+                if board[row][col] == 0 and live_neighbors == 3:
+                    # 2 signifies the cell is now live but was originally dead.
+                    board[row][col] = 2
+
+        # Get the final representation for the newly updated board.
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] > 0:
+                    board[row][col] = 1
+                else:
+                    board[row][col] = 0
+                    
+                    
+######################################################################################################
+# Follow up 2 - Infinite board
+# TC: O(mn)
+# SC: O(mn)
+
+def gameOfLifeInfinite(self, live):
+    ctr = collections.Counter((I, J)
+                              for i, j in live
+                              for I in range(i-1, i+2)
+                              for J in range(j-1, j+2)
+                              if I != i or J != j)
+    return {ij
+            for ij in ctr
+            if ctr[ij] == 3 or ctr[ij] == 2 and ij in live}
+
+def gameOfLife(self, board):
+    live = {(i, j) for i, row in enumerate(board) for j, live in enumerate(row) if live}
+    live = self.gameOfLifeInfinite(live)
+    for i, row in enumerate(board):
+        for j in range(len(row)):
+            row[j] = int((i, j) in live)
