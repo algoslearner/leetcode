3da1eea373eba6dd2c773ea1aaabc3730caaa282
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-21 10:43:08 -0700

    Create 03_design_hashset.py

---
+++ companies/extra/easy/03_design_hashset.py
@@ -0,0 +1,66 @@
+'''
+705. Design HashSet
+
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ 
+
+Example 1:
+
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+ 
+
+Constraints:
+
+0 <= key <= 106
+At most 104 calls will be made to add, remove, and contains.
+'''
+
+########################################################################################
+# use array of size 10000000
+# TC: O(N)
+# SC : O(N)
+
+class MyHashSet:
+
+    def __init__(self):
+        self.table = [0] * 10000000
+        
+
+    def add(self, key: int) -> None:
+        self.table[key] = 1
+
+    def remove(self, key: int) -> None:
+        self.table[key] = 0
+        
+
+    def contains(self, key: int) -> bool:
+        return self.table[key]
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
