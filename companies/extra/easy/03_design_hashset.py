071dc9e8c5e09e3658be513d4e83d8a4c3e7cf61
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-21 10:48:27 -0700

    Update 03_design_hashset.py

--- companies/extra/easy/03_design_hashset.py
+++ companies/extra/easy/03_design_hashset.py
@@ -35,6 +35,11 @@ Constraints:
 0 <= key <= 106
 At most 104 calls will be made to add, remove, and contains.
 '''
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
 
 ########################################################################################
 # use array of size 10000000
@@ -56,11 +61,209 @@ class MyHashSet:
 
     def contains(self, key: int) -> bool:
         return self.table[key]
-        
+
+########################################################################################
+# LinkedList used as a bucket
+# TC: O(N/ K)
+# SC : O(N + K)
+
+class MyHashSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.keyRange = 769
+        self.bucketArray = [Bucket() for i in range(self.keyRange)]
+
+    def _hash(self, key):
+        return key % self.keyRange
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].insert(key)
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].delete(key)
+
+    def contains(self, key):
+        """
+        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: bool
+        """
+        bucketIndex = self._hash(key)
+        return self.bucketArray[bucketIndex].exists(key)
 
 
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.next = nextNode
+
+class Bucket:
+    def __init__(self):
+        # a pseudo head
+        self.head = Node(0)
+
+    def insert(self, newValue):
+        # if not existed, add the new element to the head.
+        if not self.exists(newValue):
+            newNode = Node(newValue, self.head.next)
+            # set the new head.
+            self.head.next = newNode
+
+    def delete(self, value):
+        prev = self.head
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                # remove the current node
+                prev.next = curr.next
+                return
+            prev = curr
+            curr = curr.next
+
+    def exists(self, value):
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                # value existed already, do nothing
+                return True
+            curr = curr.next
+        return False
+
+########################################################################################
+# BST used as a bucket
+# TC: O(log (N/ K))
+# SC : O(N + K)
+
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.keyRange = 769
+        self.bucketArray = [Bucket() for i in range(self.keyRange)]
+
+    def _hash(self, key) -> int:
+        return key % self.keyRange
+
+    def add(self, key: int) -> None:
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].insert(key)
+
+    def remove(self, key: int) -> None:
+        """
+        :type key: int
+        :rtype: None
+        """
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].delete(key)
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: bool
+        """
+        bucketIndex = self._hash(key)
+        return self.bucketArray[bucketIndex].exists(key)
+
+class Bucket:
+    def __init__(self):
+        self.tree = BSTree()
+
+    def insert(self, value):
+        self.tree.root = self.tree.insertIntoBST(self.tree.root, value)
+
+    def delete(self, value):
+        self.tree.root = self.tree.deleteNode(self.tree.root, value)
+
+    def exists(self, value):
+        return (self.tree.searchBST(self.tree.root, value) is not None)
+
+class TreeNode:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+class BSTree:
+    def __init__(self):
+        self.root = None
+
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None or val == root.val:
+            return root
+
+        return self.searchBST(root.left, val) if val < root.val \
+            else self.searchBST(root.right, val)
+
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+
+        if val > root.val:
+            # insert into the right subtree
+            root.right = self.insertIntoBST(root.right, val)
+        elif val == root.val:
+            return root
+        else:
+            # insert into the left subtree
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+    def successor(self, root):
+        """
+        One step right and then always left
+        """
+        root = root.right
+        while root.left:
+            root = root.left
+        return root.val
+
+    def predecessor(self, root):
+        """
+        One step left and then always right
+        """
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+
+        # delete from the right subtree
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        # delete from the left subtree
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        # delete the current node
+        else:
+            # the node is a leaf
+            if not (root.left or root.right):
+                root = None
+            # the node is not a leaf and has a right child
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            # the node is not a leaf, has no right child, and has a left child
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+
+        return root
