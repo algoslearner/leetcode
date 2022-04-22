30ed7ea930f69e0c9f55be17eb27f34b444150ca
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-22 07:52:06 -0700

    Create 01_optimize_water_dist_in_a_village.py

---
+++ companies/google/leetcode/GRAPH/hard/01_optimize_water_dist_in_a_village.py
@@ -0,0 +1,124 @@
+'''
+1168. Optimize Water Distribution in a Village
+
+There are n houses in a village. We want to supply water for all the houses by building wells and laying pipes.
+
+For each house i, we can either build a well inside it directly with cost wells[i - 1] (note the -1 due to 0-indexing), or pipe in water from another well to it. The costs to lay pipes between houses are given by the array pipes where each pipes[j] = [house1j, house2j, costj] represents the cost to connect house1j and house2j together using a pipe. Connections are bidirectional, and there could be multiple valid connections between the same two houses with different costs.
+
+Return the minimum total cost to supply water to all houses.
+
+ 
+
+Example 1:
+
+
+Input: n = 3, wells = [1,2,2], pipes = [[1,2,1],[2,3,1]]
+Output: 3
+Explanation: The image shows the costs of connecting houses using pipes.
+The best strategy is to build a well in the first house with cost 1 and connect the other houses to it with cost 2 so the total cost is 3.
+Example 2:
+
+Input: n = 2, wells = [1,1], pipes = [[1,2,1],[1,2,2]]
+Output: 2
+Explanation: We can supply water with cost two using one of the three options:
+Option 1:
+  - Build a well inside house 1 with cost 1.
+  - Build a well inside house 2 with cost 1.
+The total cost will be 2.
+Option 2:
+  - Build a well inside house 1 with cost 1.
+  - Connect house 2 with house 1 with cost 1.
+The total cost will be 2.
+Option 3:
+  - Build a well inside house 2 with cost 1.
+  - Connect house 1 with house 2 with cost 1.
+The total cost will be 2.
+Note that we can connect houses 1 and 2 with cost 1 or with cost 2 but we will always choose the cheapest option. 
+ 
+
+Constraints:
+
+2 <= n <= 104
+wells.length == n
+0 <= wells[i] <= 105
+1 <= pipes.length <= 104
+pipes[j].length == 3
+1 <= house1j, house2j <= n
+0 <= costj <= 105
+house1j != house2j
+'''
+
+#######################################################################################
+# UNION FIND
+# TC: O( N+M log(N+M))
+# SC: O( N+M )
+
+class UnionFind:
+    """
+        Implementation of UnionFind without load-balancing.
+    """
+    def __init__(self, size) -> None:
+        """
+        container to hold the group id for each member
+        Note: the index of member starts from 1,
+            thus we add one more element to the container.
+        """
+        self.group = [i for i in range(size + 1)]
+        # the rank of each node for later rebalancing
+        self.rank = [0] * (size + 1)
+
+    def find(self, person: int) -> int:
+        """
+            return the group id that the person belongs to
+        """
+        if self.group[person] != person:
+            self.group[person] = self.find(self.group[person])
+        return self.group[person]
+
+    def union(self, person_1: int, person_2: int) -> bool:
+        """
+            Join the groups together.
+            return:
+                false when the two persons belong to the same group already,
+                otherwise true
+        """
+        group_1 = self.find(person_1)
+        group_2 = self.find(person_2)
+        if group_1 == group_2:
+            return False
+
+        # attach the group of lower rank to the group with higher rank
+        if self.rank[group_1] > self.rank[group_2]:
+            self.group[group_2] = group_1
+        elif self.rank[group_1] < self.rank[group_2]:
+            self.group[group_1] = group_2
+        else:
+            self.group[group_1] = group_2
+            self.rank[group_2] += 1
+
+        return True
+
+
+class Solution:
+    def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
+        ordered_edges = []
+        # add the virtual vertex (index with 0) along with the new edges.
+        for index, weight in enumerate(wells):
+            ordered_edges.append((weight, 0, index+1))
+
+        # add the existing edges
+        for house_1, house_2, weight in pipes:
+            ordered_edges.append((weight, house_1, house_2))
+
+        # sort the entire edges by their weights
+        ordered_edges.sort(key=lambda x: x[0])
+
+        # iterate through the ordered edges
+        uf = UnionFind(n)
+        total_cost = 0
+        for cost, house_1, house_2 in ordered_edges:
+            # determine if we should add the new edge to the final MST
+            if uf.union(house_1, house_2):
+                total_cost += cost
+
+        return total_cost
