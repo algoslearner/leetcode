396e40213b9bbf8b8b5330b635539552d2af3811
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-22 07:27:05 -0700

    Update 85_smallest_string_with_swaps.py

--- companies/fb/leetcode/medium/LEVEL3/85_smallest_string_with_swaps.py
+++ companies/fb/leetcode/medium/LEVEL3/85_smallest_string_with_swaps.py
@@ -40,7 +40,49 @@ Constraints:
 s only contains lower case English letters.
 '''
 
-# SOLUTION
+###########################################################################################################################
+# DISJOINT SET - UNION FIND
+# TC: O(V + E)
+# SC : O(V+ E)
+
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        class UF:
+            def __init__(self, n): self.p = list(range(n))
+            def union(self, x, y): self.p[self.find(x)] = self.find(y)
+            def find(self, x):
+                if x != self.p[x]: self.p[x] = self.find(self.p[x])
+                return self.p[x]
+        uf, res, m = UF(len(s)), [], defaultdict(list)
+        for x,y in pairs: 
+            uf.union(x,y)
+        for i in range(len(s)): 
+            m[uf.find(i)].append(s[i])
+        for comp_id in m.keys(): 
+            m[comp_id].sort(reverse=True)
+        for i in range(len(s)): 
+            res.append(m[uf.find(i)].pop())
+        return ''.join(res)
+'''
+# LOGIC
+# https://leetcode.com/problems/smallest-string-with-swaps/discuss/387524/Short-Python-Union-find-solution-w-Explanation
+
+The core of the idea is that if (0, 1) is an exchange pair and (0, 2) is an exchange pair, then any 2 in (0, 1, 2) can be exchanged.
+
+This implies, we can build connected components where each component is a list of indices that can be exchanged with any of them. In Union find terms, we simply iterate through each pair, and do a union on the indices in the pair.
+At the end of the union of all the pairs, we have built connected component of indices that can be exchanged with each other.
+
+Then we build a sorted list of characters for every connected component.
+
+The final step is, we iterate through all the indices, and for each index we locate its component id and find the sorted list correspondng to that component and grab the next lowest character from that list.
+
+This way for every index, we find the lowest possible character that can be exchanged and fitted there.
+'''
+
+###########################################################################################################################
+# DFS
+# TC: O(n log n)
+# SC: O(n)
 # https://leetcode.com/problems/smallest-string-with-swaps/discuss/753004/Clean-Python-DFS-or-O(-n-log-n-)
 
 class Solution:
