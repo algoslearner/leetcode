4742af6e082b93245e3aae00a4121c193d1cc95c
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-22 06:56:48 -0700

    Update 04_number_of_connected_components.py

--- companies/google/leetcode/GRAPH/medium/04_number_of_connected_components.py
+++ companies/google/leetcode/GRAPH/medium/04_number_of_connected_components.py
@@ -72,3 +72,31 @@ class Solution:
             uf.union(*edge)
             
         return uf.count     
+##############################################################################################################
+# DFS of graph
+# TC : O(E + V)
+# SC : O(E + V)
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        count = 0
+        graph = [[] for _ in range(n)]
+        seen = [False for _ in range(n)]
+        
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            
+        def dfs(node):
+            for adj in graph[node]:
+                if not seen[adj]:
+                    seen[adj] = True
+                    dfs(adj)
+                    
+        for i in range(n):
+            if not seen[i]:
+                count += 1
+                seen[i] = True
+                dfs(i)
+                
+        return count    
