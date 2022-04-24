949ff9f7c3cc4e5c545a372d6df17ff8e0f59171
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 15:53:58 -0700

    Create 11_demolition_robot.py

---
+++ companies/amazon/OA/11_demolition_robot.py
@@ -0,0 +1,15 @@
+# Amazon OA assignment: https://leetcode.com/discuss/interview-question/1033264/Amazon-or-OA-or-1-year-Experienced-for-SDE1
+
+'''
+Demolition Robot
+Given a matrix with values 0 (trenches) , 1 (flat) , and 9 (obstacle) you have to find minimum distance to reach 9 (obstacle). If not possible then return -1.
+The demolition robot must start at the top left corner of the matrix, which is always flat, and can move on block up, down, right, left.
+The demolition robot cannot enter 0 trenches and cannot leave the matrix.
+Sample Input :
+[1, 0, 0],
+[1, 0, 0],
+[1, 9, 1]]
+Sample Output :
+3
+This question can be solved by using BFS or DFS.
+'''
