a4671c04c09c14f2dd76777daa610232536ee723
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 05:21:41 -0700

    Update 04_count_unique_chracters_of_all_substrings_of_given_string.py

--- companies/amazon/OA/04_count_unique_chracters_of_all_substrings_of_given_string.py
+++ companies/amazon/OA/04_count_unique_chracters_of_all_substrings_of_given_string.py
@@ -59,25 +59,26 @@ class Solution:
 
 ##############################################################################################################################
 # HASHMAP : https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/discuss/1823865/Very-Easy-to-understand-Dict-Solution-O(n)
-# TC: O(n)
+# TC: O(n^2)
 # SC: O(n)
 
 class Solution:
-  def uniqueLetterString(self, s: str) -> int:
-    hashMap = {}   # {char: [positions]}
-    for i in range(len(s)):
-        char = s[i]
-        if char in hashMap:
-            hashMap[char].append(i)
-        else:
-            hashMap[char] = [-1]
-            hashMap[char].append(i)
+    def uniqueLetterString(self, s: str) -> int:
+        hashMap = {}   # {char: [positions]}
+        for i in range(len(s)):
+            char = s[i]
+            if char in hashMap:
+                hashMap[char].append(i)
+            else:
+                hashMap[char] = [-1]
+                hashMap[char].append(i)
+                
+        count = 0
+        for key in hashMap:
+            positions = hashMap[key]
             
-    count = 0
-    for key in hashMap:
-        positions = hashMap[key]
-        
-        positions.append(len(s))
-        for i in range(1, len(positions) - 1):
-            count = count + (positions[i] - positions[i-1]) * (positions[i+1] - positions[i])
-    return count
+            positions.append(len(s))
+            for i in range(1, len(positions) - 1):
+                count = count + (positions[i] - positions[i-1]) * (positions[i+1] - positions[i])
+                
+        return count
