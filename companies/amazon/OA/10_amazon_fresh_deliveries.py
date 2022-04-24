18339131388a62e62f23249eaedf8bf6a61076a3
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 16:03:10 -0700

    Update 10_amazon_fresh_deliveries.py

--- companies/amazon/OA/10_amazon_fresh_deliveries.py
+++ companies/amazon/OA/10_amazon_fresh_deliveries.py
@@ -11,16 +11,4 @@ Sample Output :
 [ [1, -1], [1 , 2] ]
 Output list can be in any order.
 This question was basically K closest points to the origin (0,0) with added tie condition.
-
-Demolition Robot
-Given a matrix with values 0 (trenches) , 1 (flat) , and 9 (obstacle) you have to find minimum distance to reach 9 (obstacle). If not possible then return -1.
-The demolition robot must start at the top left corner of the matrix, which is always flat, and can move on block up, down, right, left.
-The demolition robot cannot enter 0 trenches and cannot leave the matrix.
-Sample Input :
-[1, 0, 0],
-[1, 0, 0],
-[1, 9, 1]]
-Sample Output :
-3
-This question can be solved by using BFS or DFS.
 '''
