1bd3a9e14fc7c39597006c9647743cff7eaad9c6
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 05:44:13 -0700

    Update 05_find_all_combination_of_numbers_sum_to_target.py

--- companies/amazon/OA/05_find_all_combination_of_numbers_sum_to_target.py
+++ companies/amazon/OA/05_find_all_combination_of_numbers_sum_to_target.py
@@ -7,6 +7,45 @@ Should be O(N^3). I did not pass the tests per TLE
 '''
 
 ########################################################################################################################
+# Related leetcode question: https://leetcode.com/problems/combination-sum/
+'''
+39. Combination Sum
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+All elements of candidates are distinct.
+1 <= target <= 500
+'''
+
+
+########################################################################################################################
 # Related leetcode question: https://leetcode.com/problems/4sum-ii/
 
 '''
@@ -39,6 +78,32 @@ n == nums4.length
 1 <= n <= 200
 -228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
 '''
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        results = []
+
+        def backtrack(remain, comb, start):
+            if remain == 0:
+                # make a deep copy of the current combination
+                results.append(list(comb))
+                return
+            elif remain < 0:
+                # exceed the scope, stop exploration.
+                return
+
+            for i in range(start, len(candidates)):
+                # add the number into the combination
+                comb.append(candidates[i])
+                # give the current number another chance, rather than moving on
+                backtrack(remain - candidates[i], comb, i)
+                # backtrack, remove the number from the combination
+                comb.pop()
+
+        backtrack(target, [], 0)
+
+        return results
 
 ########################################################################################################################
 # USING DICT
