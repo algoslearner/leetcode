41e5d582e8cb8820e466d59870db8756e98e4e1b
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 16:05:33 -0700

    Create 13_no_of_swaps_in_selection_sort.py

---
+++ companies/amazon/OA/13_no_of_swaps_in_selection_sort.py
@@ -0,0 +1,6 @@
+'''
+I recently gave Amazon OA
+
+no of swaps in selection sort. --- same leetcode question no of smaller elements after self -- twisted merge sort
+https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+'''
