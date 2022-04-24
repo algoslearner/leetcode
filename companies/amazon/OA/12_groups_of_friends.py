ceb5203434ee86e381149c007f041ff42cee0d39
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 16:04:21 -0700

    Create 12_groups_of_friends.py

---
+++ companies/amazon/OA/12_groups_of_friends.py
@@ -0,0 +1,12 @@
+'''
+HI all,
+
+I recently gave Amazon OA
+Two questions were asked -
+
+no of swaps in selection sort. --- same leetcode question no of smaller elements after self -- twisted merge sort
+no of groups of friends {audio subscriptions} --- same leetcode question -- friends of friend --- Dfs
+
+https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+https://leetcode.com/problems/number-of-provinces/
+'''
