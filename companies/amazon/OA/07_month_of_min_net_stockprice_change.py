247c35e0b1244bed815ac30d6f286661a4f85084
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 21:43:36 -0700

    Update 07_month_of_min_net_stockprice_change.py

--- companies/amazon/OA/07_month_of_min_net_stockprice_change.py
+++ companies/amazon/OA/07_month_of_min_net_stockprice_change.py
@@ -1,23 +1,47 @@
 ####################################################################################################
 # Amazon OA question: https://leetcode.com/discuss/interview-question/1858858/amazon-oa
+# https://leetcode.com/discuss/interview-question/1978286/Amazon-oror-OA-oror-Find-Earliest-Month-Stock-Problem-oror-24th-April-2022
 ####################################################################################################
 
 '''
-Given the stock prices of n months, the net price change for the i-th month is defined as the absolute difference between the average of stock prices for the first i months and for the remaining (n−i) months where 1≤i≤n.
-Note that these averages are rounded down to an integer.
+The interns at Amazon were asked to review the company's stock value over a period. Given the stock prices of n months, the net price change for the ith month is defined as the absolute difference between the average of stock prices for the first i : months and for the remaining (n - i) months where 1 si<n. Note that these averages are rounded down to an integer.
 
-The average of a set of integers here is defined as the sum of integers divided by the number of integers, rounded down to the nearest integer.
-For example, the average of [1, 2, 3, 4] is the floor of (1 + 2 + 3 + 4) / 4 = 2.5 and the floor of 2.5 is 2.
+Given an array of stock prices, find the month at which the net price change is minimum. If there are several such months, return the earliest month.
 
-Given an array of stock prices, find the month at which the net price change is minimum.
-If there are several such months, return the earliest month.
+Note: The average of a set of integers here is defined as the sum of integers divided by the number of integers, rounded down to the nearest integer. For example, the average of [1, 2, 3, 4] is the floor of (1 + 2+ 3+ 4)/4= 2.5 and the floor of 2.5 is 2.
 
-Constraints:
+Example :
 
-2 <= n <= 10^5
+stockPrice = [1, 3, 2, 4,5]
+answer : 2
 
-1 <= stockPrice[i] <= 10^9
+stockPrice = [1,1,1,1,1,1]
+answer : 1
+
+Solution : (15/15) ALL TEST CASES PASSED
 '''
+
+def findEarliestMonth(stockPrice):
+    optimalMonth=0
+    price=float('inf')
+    totalSum=sum(stockPrice)
+    total=0
+    n=len(stockPrice)
+    for month,value in enumerate(stockPrice,start=1):
+        if(month==n):
+            break
+        total+=value
+        k=total//month
+        p=(totalSum-total)//(n-month)
+        net=abs(k-p)
+        if(net<price):
+            price=net
+            optimalMonth=month
+    return optimalMonth
+
+##########################################################################################
+# BACKUP SOLUTIONS LISTED
+##########################################################################################
 
 #initialize month variable with 0
     month=0
