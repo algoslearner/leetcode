a6811421e58e1a260fcb4a039e4da02c0a2c34de
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-24 21:50:05 -0700

    Create 20_flight_media.py

---
+++ companies/amazon/OA/20_flight_media.py
@@ -0,0 +1,12 @@
+'''
+# https://leetcode.com/discuss/interview-question/1227869/Amazon-OA
+
+Giving back to the community! Recently did the OA for Amazon. I prepped by using this post
+
+1. Flight Media - basically two sums, but you had to account for multiple pairs that could add up to the target
+2. Subscriber Groups - similar to https://leetcode.com/problems/number-of-provinces/, find the number of unique friend groups
+Each question followed up with an explanation on approach and complexity. This was done on Hackerrank.
+'''
+
+# Flight Media - basically two sums, but you had to account for multiple pairs that could add up to the target
+#  https://leetcode.com/problems/two-sum-less-than-k/
