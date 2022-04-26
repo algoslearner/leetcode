8224e813a7cce6d93715e9f13b32c90a8e50dd8b
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-26 08:59:26 -0700

    Create 25_path_sum_iii.py

---
+++ companies/google/leetcode/TREE/medium/25_path_sum_iii.py
@@ -0,0 +1,75 @@
+# https://leetcode.com/problems/path-sum-iii/
+'''
+437. Path Sum III
+
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+Output: 3
+Explanation: The paths that sum to 8 are shown.
+Example 2:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 1000].
+-109 <= Node.val <= 109
+-1000 <= targetSum <= 1000
+'''
+
+##############################################################################################
+# powerful "prefix sum technique": one pass + linear time complexity.
+# Prefix sum is a sum of the current value with all previous elements starting from the beginning of the structure.
+# TC: O(N)
+# SC: O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        def preorder(node: TreeNode, curr_sum) -> None:
+            nonlocal count
+            if not node:
+                return
+            
+            # current prefix sum
+            curr_sum += node.val
+            
+            # here is the sum we are looking for
+            if curr_sum == k:
+                count += 1
+            
+            count += h[curr_sum - k]
+            
+            # add curr_sum to hashmap, to use it for child nodes processing
+            h[curr_sum] += 1
+            
+            # process left subtree
+            preorder(node.left, curr_sum)
+            
+            # process right subtree
+            preorder(node.right, curr_sum)
+            
+            # remove curr_sum from hashmap, to use it for parallel subtrees
+            h[curr_sum] -= 1
+        
+        count = 0
+        k = targetSum
+        h = defaultdict(int)
+        
+        preorder(root, 0)
+        return count
