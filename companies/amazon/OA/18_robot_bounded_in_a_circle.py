141ae47bf83f5a7522bffba23786f81d8475fac8
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-26 14:40:47 -0700

    Update 18_robot_bounded_in_a_circle.py

--- companies/amazon/OA/18_robot_bounded_in_a_circle.py
+++ companies/amazon/OA/18_robot_bounded_in_a_circle.py
@@ -1,1 +1,7 @@
 # https://leetcode.com/problems/robot-bounded-in-circle/
+'''
+I got the below questions for Amazon online assessment (31-JAN-2021):
+https://leetcode.com/discuss/interview-question/974912/amazon-phone-find-all-combinations-of-numbers-that-sum-to-a-target
+https://leetcode.com/problems/robot-bounded-in-circle/
+This is for experienced professionals
+'''
