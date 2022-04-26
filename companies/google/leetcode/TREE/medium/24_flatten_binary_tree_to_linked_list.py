5eecf19cddfc54336c07f23f6ff7a847bd5ccea6
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-25 18:29:18 -0700

    Create 24_flatten_binary_tree_to_linked_list.py

---
+++ companies/google/leetcode/TREE/medium/24_flatten_binary_tree_to_linked_list.py
@@ -0,0 +1,184 @@
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+'''
+114. Flatten Binary Tree to Linked List
+
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+Example 1:
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+'''
+
+###########################################################################################################
+# RECURSION
+# TC: O(N)
+# SC: O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    
+    def flattenTree(self, node):
+        
+        # Handle the null scenario
+        if not node:
+            return None
+        
+        # For a leaf node, we simply return the
+        # node as is.
+        if not node.left and not node.right:
+            return node
+        
+        # Recursively flatten the left subtree
+        leftTail = self.flattenTree(node.left)
+        
+        # Recursively flatten the right subtree
+        rightTail = self.flattenTree(node.right)
+        
+        # If there was a left subtree, we shuffle the connections
+        # around so that there is nothing on the left side
+        # anymore.
+        if leftTail:
+            leftTail.right = node.right
+            node.right = node.left
+            node.left = None
+        
+        # We need to return the "rightmost" node after we are
+        # done wiring the new connections. 
+        return rightTail if rightTail else leftTail
+    
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        self.flattenTree(root)
+        
+###########################################################################################################
+# ITERATION using stack
+# TC: O(N)
+# SC: O(N)
+
+import collections
+class Solution:
+    
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        # Handle the null scenario
+        if not root:
+            return None
+        
+        START, END = 1, 2
+        
+        tailNode = None
+        stack = collections.deque([(root, START)])
+        
+        while stack:
+            
+            currentNode, recursionState = stack.pop()
+            
+            # We reached a leaf node. Record this as a tail
+            # node and move on.
+            if not currentNode.left and not currentNode.right:
+                tailNode = currentNode
+                continue
+            
+            # If the node is in the START state, it means we still
+            # haven't processed it's left child yet.
+            if recursionState == START:
+                
+                # If the current node has a left child, we add it
+                # to the stack AFTER adding the current node again
+                # to the stack with the END recursion state. 
+                if currentNode.left:          
+                    stack.append((currentNode, END))
+                    stack.append((currentNode.left, START))
+                elif currentNode.right:
+                    
+                    # In case the current node didn't have a left child
+                    # we will add it's right child
+                    stack.append((currentNode.right, START))
+            else:
+                # If the current node is in the END recursion state,
+                # that means we did process one of it's children. Left
+                # if it existed, else right.
+                rightNode = currentNode.right
+                
+                # If there was a left child, there must have been a leaf
+                # node and so, we would have set the tailNode
+                if tailNode:
+                    
+                    # Establish the proper connections. 
+                    tailNode.right = currentNode.right
+                    currentNode.right = currentNode.left
+                    currentNode.left = None
+                    rightNode = tailNode.right
+                
+                if rightNode:
+                    stack.append((rightNode, START))                
+
+###########################################################################################################
+# ITERATION without using extra space
+# TC: O(N)
+# SC: O(1)
+
+class Solution:
+    
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        # Handle the null scenario
+        if not root:
+            return None
+        
+        node = root
+        while node:
+            
+            # If the node has a left child
+            if node.left:
+                
+                # Find the rightmost node
+                rightmost = node.left
+                while rightmost.right:
+                    rightmost = rightmost.right
+                
+                # rewire the connections
+                rightmost.right = node.right
+                node.right = node.left
+                node.left = None
+            
+            # move on to the right side of the tree
+            node = node.right
