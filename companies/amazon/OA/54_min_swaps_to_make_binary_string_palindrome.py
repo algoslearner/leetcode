d4224add80ac933eb048d2e8fb95455aa3f0783c
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-28 13:08:43 -0700

    Create 54_min_swaps_to_make_binary_string_palindrome.py

---
+++ companies/amazon/OA/54_min_swaps_to_make_binary_string_palindrome.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/discuss/interview-question/1982251/Amazon-OA-or-USA-or-SDE2-or-Minimum-Swaps-To-Make-A-Binary-String-Palindrome
+'''
+'''
+
+# SOLUTION REFER
+# https://stackoverflow.com/questions/72017854/minimum-number-of-swaps-required-to-make-a-binary-string-palindrome
+
+# https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-binary-string-alternating/
+# The both are different. For instance if the input is "1010", the minimum swaps to make alternating returns 0 where as the current palindrome question should return 1.
