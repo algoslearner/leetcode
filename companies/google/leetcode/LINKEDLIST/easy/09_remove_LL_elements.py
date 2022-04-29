d6b19dab9f384abcb82a5d1a9787b70f206cb2e4
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-29 16:44:03 -0700

    Update 09_remove_LL_elements.py

--- companies/google/leetcode/LINKEDLIST/easy/09_remove_LL_elements.py
+++ companies/google/leetcode/LINKEDLIST/easy/09_remove_LL_elements.py
@@ -74,3 +74,13 @@ class Solution:
                     node.next = rec(node.next, val)
                 return node
         return rec(head, val)
+
+#OR
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        
+        head.next = self.removeElements(head.next, val)
+        return head.next if head.val == val else head
