3187e48c9522d5502e192a3943f1f1255336649a
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-29 06:40:18 -0700

    Create 10_hamming_distance.py

---
+++ companies/google/leetcode/easy/10_hamming_distance.py
@@ -0,0 +1,54 @@
+# https://leetcode.com/problems/hamming-distance/
+'''
+461. Hamming Distance
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, return the Hamming distance between them.
+
+ 
+
+Example 1:
+
+Input: x = 1, y = 4
+Output: 2
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+The above arrows point to positions where the corresponding bits are different.
+Example 2:
+
+Input: x = 3, y = 1
+Output: 1
+ 
+
+Constraints:
+
+0 <= x, y <= 231 - 1
+'''
+
+#####################################################################################################################
+# Inbuilt bit counting function
+# TC: O(1)
+# SC: O(1)
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return bin(x ^ y).count('1')
+      
+#####################################################################################################################
+# Bit shift
+# TC: O(1)
+# SC: O(1)
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
+        distance = 0
+        while xor:
+            # mask out the rest bits
+            if xor & 1:
+                distance += 1
+            xor = xor >> 1
+        return distance
