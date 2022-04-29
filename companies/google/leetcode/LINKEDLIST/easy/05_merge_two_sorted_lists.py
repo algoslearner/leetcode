edaaf854c1c2487fbf5e18642937eaec4ce8fe88
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-29 13:54:07 -0700

    Create 05_merge_two_sorted_lists.py

---
+++ companies/google/leetcode/LINKEDLIST/easy/05_merge_two_sorted_lists.py
@@ -0,0 +1,79 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/
+'''
+21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+###############################################################################################################
+# RECURSION
+# TC: O(n + m)
+# SC: O(n + m)
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+          
+###############################################################################################################
+# ITERATION
+# TC: O(n + m)
+# SC: O(1)
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        prehead = ListNode(-1) # unchanging reference node ahead of return node
+        prev = prehead
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        
+        prev.next = list1 if list1 is not None else list2
+        return prehead.next
