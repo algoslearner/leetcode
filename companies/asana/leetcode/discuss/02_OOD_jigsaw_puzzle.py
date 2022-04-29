71b4e939be0bd45cdc13d4087acc8d4cc8808898
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-04-28 18:57:47 -0700

    Create 02_OOD_jigsaw_puzzle.py

---
+++ companies/asana/leetcode/discuss/02_OOD_jigsaw_puzzle.py
@@ -0,0 +1,3 @@
+# More like a design question. Given a Piece class and a Side class, design a method solve(pieces[]) which returns a solved puzzle. I had to design the data structure for solved puzzle and how I would write the solve method.
+
+# SOLUTION: https://massivetechinterview.blogspot.com/2015/07/implement-jigsaw-puzzle-kodeknight.html
