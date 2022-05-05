6a9dded24732d875d61883f4551958c2713bd31e
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-05 08:10:25 -0700

    Create count_nodes_equal_to_sum_of_descendants.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL1/count_nodes_equal_to_sum_of_descendants.py
@@ -0,0 +1,69 @@
+# https://leetcode.com/problems/count-nodes-equal-to-sum-of-descendants/
+'''
+1973. Count Nodes Equal to Sum of Descendants
+
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the sum of the values of its descendants.
+
+A descendant of a node x is any node that is on the path from node x to some leaf node. The sum is considered to be 0 if the node has no descendants.
+
+ 
+
+Example 1:
+
+
+Input: root = [10,3,4,2,1]
+Output: 2
+Explanation:
+For the node with value 10: The sum of its descendants is 3+4+2+1 = 10.
+For the node with value 3: The sum of its descendants is 2+1 = 3.
+Example 2:
+
+
+Input: root = [2,3,null,2,null]
+Output: 0
+Explanation:
+No node has a value that is equal to the sum of its descendants.
+Example 3:
+
+
+Input: root = [0]
+Output: 1
+For the node with value 0: The sum of its descendants is 0 since it has no descendants.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+0 <= Node.val <= 105
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+########################################################################################
+# what would you modify if the question is asking for 'average' of descendants instead of 'sum' of descendants? 
+# I saw this was being asked at FB phone interview but not sure what to tweak. 
+# should we add another variable to count how many nodes so far?
+
+########################################################################################
+# DFS
+# TC: O(N)
+# SC: O(n)
+
+class Solution:
+    def equalToDescendants(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node):
+            """Return sum of nodes' value on sub-tree."""
+            nonlocal ans
+            if not node: return 0 
+            sm = dfs(node.left) + dfs(node.right)
+            if sm == node.val: ans += 1
+            return sm + node.val 
+        
+        ans = 0 
+        dfs(root)
+        return ans
