ce45c47e91bcd84fa644ff4fd9650eeb9fe343f5
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-06 04:05:07 -0700

    Create 08_remove_all_adjacent_duplicates_in_a_string_ii.py

---
+++ companies/fb/leetcode/easy/08_remove_all_adjacent_duplicates_in_a_string_ii.py
@@ -0,0 +1,61 @@
+# https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+'''
+1209. Remove All Adjacent Duplicates in String II
+Medium: Fb 8, Bloomberg 26
+
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+2 <= k <= 104
+s only contains lower case English letters.
+'''
+
+# https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/discuss/392933/JavaC%2B%2BPython-Two-Pointers-and-Stack-Solution
+######################################################################################################
+# STACK
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+                if stack[-1][1] == k: stack.pop()
+            else:
+                stack.append([c, 1])
+        return "".join(c * count for c, count in stack)
+      
+######################################################################################################
+# TWO POINTERS
+# TC: O(N)
+# SC: O(1) or O(N)?
+
