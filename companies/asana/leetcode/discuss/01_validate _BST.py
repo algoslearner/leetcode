672b31b0b66dacc79c6d3e93d306e3b711e0917d
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-08 11:48:47 -0700

    Update 01_validate _BST.py

--- companies/asana/leetcode/discuss/01_validate _BST.py
+++ companies/asana/leetcode/discuss/01_validate _BST.py
@@ -1,1 +1,85 @@
 # Verify if a given binary tree is a BST. Duplicate values appear towards the left.
+
+# https://leetcode.com/problems/validate-binary-search-tree/
+'''
+98. Validate Binary Search Tree
+
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+##############################################################################################
+# ITERATIVE
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = []
+        prev = -math.inf
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            
+            # next element smaller than previous one
+            if root.val <= prev:
+                return False
+            prev = root.val
+            root = root.right
+        
+        return True
+      
+##############################################################################################
+# RECURSION
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+
+        def inorder(root):
+            if not root:
+                return True
+            if not inorder(root.left):
+                return False
+            if root.val <= self.prev:
+                return False
+            self.prev = root.val
+            return inorder(root.right)
+
+        self.prev = -math.inf
+        return inorder(root)==================9] 
