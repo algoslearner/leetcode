cf9c22f95b7c2dbe04859d7827175c90155362ec
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-18 13:50:06 -0700

    Update 57_N-ary_tree_level_order_traversal.py

--- companies/google/leetcode/TREE/medium/LEVEL2/57_N-ary_tree_level_order_traversal.py
+++ companies/google/leetcode/TREE/medium/LEVEL2/57_N-ary_tree_level_order_traversal.py
@@ -56,3 +56,24 @@ class Solution:
                 queue.extend(node.children)
             result.append(level)
         return result
+       
+####################################################################################################
+# RECURSION
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def levelOrder(self, root: Optional['Node']) -> List[List[int]]:
+
+        def traverse_node(node, level):
+            if len(result) == level:
+                result.append([])
+            result[level].append(node.val)
+            for child in node.children:
+                traverse_node(child, level + 1)
+
+        result = []
+
+        if root is not None:
+            traverse_node(root, 0)
+        return result
