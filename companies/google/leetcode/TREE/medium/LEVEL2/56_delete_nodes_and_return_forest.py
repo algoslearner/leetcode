a64108db83d8d40051de3636a7037e1490e284bf
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-18 13:20:43 -0700

    Create 56_delete_nodes_and_return_forest.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL2/56_delete_nodes_and_return_forest.py
@@ -0,0 +1,60 @@
+# https://leetcode.com/problems/delete-nodes-and-return-forest/
+'''
+1110. Delete Nodes And Return Forest
+
+Given the root of a binary tree, each node in the tree has a distinct value.
+
+After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+
+Return the roots of the trees in the remaining forest. You may return the result in any order.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+Example 2:
+
+Input: root = [1,2,4,null,3], to_delete = [3]
+Output: [[1,2,4]]
+ 
+
+Constraints:
+
+The number of nodes in the given tree is at most 1000.
+Each node has a distinct value between 1 and 1000.
+to_delete.length <= 1000
+to_delete contains distinct values between 1 and 1000.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+###################################################################################################
+# Recursion
+# TC: O(N)
+# SC: O(h + N), where h is the height of the tree
+# https://leetcode.com/problems/delete-nodes-and-return-forest/discuss/328853/JavaC%2B%2BPython-Recursion-Solution
+# If a node is root (has no parent) and isn't deleted, then will we add it to the result.
+
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        to_delete_set = set(to_delete)
+        res = []
+
+        def helper(root, is_root):
+            if not root: return None
+            root_deleted = root.val in to_delete_set
+            if is_root and not root_deleted:
+                res.append(root)
+            root.left = helper(root.left, root_deleted)
+            root.right = helper(root.right, root_deleted)
+            return None if root_deleted else root
+        helper(root, True)
+        return res
