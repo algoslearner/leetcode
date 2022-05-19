81fdf7581bb94013f203c6e7599636b0ab954681
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-19 11:29:13 -0700

    Create 63_pseudo_palindromic_paths.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL2/63_pseudo_palindromic_paths.py
@@ -0,0 +1,95 @@
+# https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/
+'''
+1457. Pseudo-Palindromic Paths in a Binary Tree
+
+Given a binary tree where node values are digits from 1 to 9. A path in the binary tree is said to be pseudo-palindromic if at least one permutation of the node values in the path is a palindrome.
+
+Return the number of pseudo-palindromic paths going from the root node to leaf nodes.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [2,3,1,3,1,null,1]
+Output: 2 
+Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the red path [2,3,3], the green path [2,1,1], and the path [2,3,1]. Among these paths only red path and green path are pseudo-palindromic paths since the red path [2,3,3] can be rearranged in [3,2,3] (palindrome) and the green path [2,1,1] can be rearranged in [1,2,1] (palindrome).
+Example 2:
+
+
+
+Input: root = [2,1,1,1,3,null,null,null,null,null,1]
+Output: 1 
+Explanation: The figure above represents the given binary tree. There are three paths going from the root node to leaf nodes: the green path [2,1,1], the path [2,1,3,1], and the path [2,1]. Among these paths only the green path is pseudo-palindromic since [2,1,1] can be rearranged in [1,2,1] (palindrome).
+Example 3:
+
+Input: root = [9]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+1 <= Node.val <= 9
+'''
+
+#####################################################################################################
+'''
+The problem consists of two subproblems:
+Traverse the tree to build all root-to-leaf paths.
+For each root-to-leaf path, check if it's a pseudo-palindromic path or not.
+'''
+
+#####################################################################################################
+# iterative preorder
+# TC: O(N)
+# SC: O(h)
+
+class Solution:
+    def pseudoPalindromicPaths (self, root: TreeNode) -> int:
+        count = 0
+        
+        stack = [(root, 0) ]
+        while stack:
+            node, path = stack.pop()
+            if node is not None:
+                # compute occurences of each digit 
+                # in the corresponding register
+                path = path ^ (1 << node.val)
+                # if it's a leaf, check if the path is pseudo-palindromic
+                if node.left is None and node.right is None:
+                    # check if at most one digit has an odd frequency
+                    if path & (path - 1) == 0:
+                        count += 1
+                else:
+                    stack.append((node.right, path))
+                    stack.append((node.left, path))
+        
+        return count
+      
+#####################################################################################################
+# recursive preorder
+# TC: O(N)
+# SC: O(h)
+
+class Solution:
+    def pseudoPalindromicPaths (self, root: TreeNode) -> int:
+        def preorder(node, path):
+            nonlocal count
+            if node:
+                # compute occurences of each digit 
+                # in the corresponding register
+                path = path ^ (1 << node.val)
+                # if it's a leaf, check if the path is pseudo-palindromic
+                if node.left is None and node.right is None:
+                    # check if at most one digit has an odd frequency
+                    if path & (path - 1) == 0:
+                        count += 1
+                else:                    
+                    preorder(node.left, path)
+                    preorder(node.right, path) 
+        
+        count = 0
+        preorder(root, 0)
+        return count
