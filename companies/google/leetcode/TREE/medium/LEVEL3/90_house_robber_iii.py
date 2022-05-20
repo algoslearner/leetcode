d3e9924f5d3187a1c8f2c916e4b93ca118178e6e
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-20 13:43:30 -0700

    Create 90_house_robber_iii.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL3/90_house_robber_iii.py
@@ -0,0 +1,60 @@
+# https://leetcode.com/problems/house-robber-iii/
+'''
+337. House Robber III
+amazon 6, google 4
+
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
+
+Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,2,3,null,3,null,1]
+Output: 7
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+
+
+Input: root = [3,4,5,1,3,null,1]
+Output: 9
+Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+0 <= Node.val <= 104
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#################################################################################################
+# RECUSRION
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def helper(node):
+            # return [rob this node, not rob this node]
+            if not node:
+                return (0, 0)
+            left = helper(node.left)
+            right = helper(node.right)
+            # if we rob this node, we cannot rob its children
+            rob = node.val + left[1] + right[1]
+            # else we could choose to either rob its children or not
+            not_rob = max(left) + max(right)
+            return [rob, not_rob]
+
+        return max(helper(root))
