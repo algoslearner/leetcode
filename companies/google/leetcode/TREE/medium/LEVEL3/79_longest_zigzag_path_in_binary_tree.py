1760389c528c523e0766bac09956540ca9494382
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-20 12:46:30 -0700

    Create 79_longest_zigzag_path_in_binary_tree.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL3/79_longest_zigzag_path_in_binary_tree.py
@@ -0,0 +1,104 @@
+# https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/
+'''
+1372. Longest ZigZag Path in a Binary Tree
+
+You are given the root of a binary tree.
+
+A ZigZag path for a binary tree is defined as follow:
+
+Choose any node in the binary tree and a direction (right or left).
+If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
+Change the direction from right to left or from left to right.
+Repeat the second and third steps until you can't move in the tree.
+Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
+
+Return the longest ZigZag path contained in that tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,1,1,1,null,null,1,1,null,1,null,null,null,1,null,1]
+Output: 3
+Explanation: Longest ZigZag path in blue nodes (right -> left -> right).
+Example 2:
+
+
+Input: root = [1,1,1,null,1,null,null,1,1,null,1]
+Output: 4
+Explanation: Longest ZigZag path in blue nodes (left -> right -> left -> right).
+Example 3:
+
+Input: root = [1]
+Output: 0
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 5 * 104].
+1 <= Node.val <= 100
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+##################################################################################################
+# REURSION
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        self.dfs(root.left,True,1)
+        self.dfs(root.right,False,1)
+        return self.res
+    
+    def dfs(self,node,isLeft,depth):
+        if not node: return
+        self.res = max(self.res,depth)
+        
+        if isLeft:
+            self.dfs(node.left,True,1)
+            self.dfs(node.right,False,depth+1)
+        else:
+            self.dfs(node.left,True,depth+1)
+            self.dfs(node.right,False,1)
+            
+ 
+##################################################################################################
+# ITERATION
+# TC: O(N)
+# SC: O(N)
+# https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/discuss/1536952/Python-or-Recursion-sol-and-Iteration-sol-Easy-and-Clean
+
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        if not root: return res
+        q= deque() # node, isLeft, depth
+        if root.left: 
+            q.append((root.left,True,1))
+        if root.right:
+            q.append((root.right, False,1))
+        
+        while q:
+            node,isleft,depth = q.popleft()
+            res = max(res,depth)
+            if isleft:
+                if node.right: 
+                    q.append((node.right,False,depth+1))
+                if node.left:
+                    q.append((node.left,True,1))
+                
+            else:
+                if node.left:
+                    q.append((node.left,True,depth+1))
+                if node.right:
+                    q.append((node.right,False,1))
+        return res
