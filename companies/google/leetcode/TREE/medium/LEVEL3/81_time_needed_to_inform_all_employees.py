94e6ac3d17481d4dd08b60d6ef32ab6e45014492
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-20 12:56:44 -0700

    Create 81_time_needed_to_inform_all_employees.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL3/81_time_needed_to_inform_all_employees.py
@@ -0,0 +1,73 @@
+# https://leetcode.com/problems/time-needed-to-inform-all-employees/
+# google 12, amazon 5
+'''
+1376. Time Needed to Inform All Employees
+
+A company has n employees with a unique ID for each employee from 0 to n - 1. The head of the company is the one with headID.
+
+Each employee has one direct manager given in the manager array where manager[i] is the direct manager of the i-th employee, manager[headID] = -1. Also, it is guaranteed that the subordination relationships have a tree structure.
+
+The head of the company wants to inform all the company employees of an urgent piece of news. He will inform his direct subordinates, and they will inform their subordinates, and so on until all employees know about the urgent news.
+
+The i-th employee needs informTime[i] minutes to inform all of his direct subordinates (i.e., After informTime[i] minutes, all his direct subordinates can start spreading the news).
+
+Return the number of minutes needed to inform all the employees about the urgent news.
+
+ 
+
+Example 1:
+
+Input: n = 1, headID = 0, manager = [-1], informTime = [0]
+Output: 0
+Explanation: The head of the company is the only employee in the company.
+Example 2:
+
+
+Input: n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0]
+Output: 1
+Explanation: The head of the company with id = 2 is the direct manager of all the employees in the company and needs 1 minute to inform them all.
+The tree structure of the employees in the company is shown.
+ 
+
+Constraints:
+
+1 <= n <= 105
+0 <= headID < n
+manager.length == n
+0 <= manager[i] < n
+manager[headID] == -1
+informTime.length == n
+0 <= informTime[i] <= 1000
+informTime[i] == 0 if employee i has no subordinates.
+It is guaranteed that all the employees can be informed.
+'''
+
+# https://leetcode.com/problems/time-needed-to-inform-all-employees/discuss/532560/JavaC%2B%2BPython-DFS
+
+########################################################################################################
+# Bottom-up DFS : faster
+# I think bottom-Up is faster because O(N) time is saved in not making a graph like we are doing it in the Top-Down approach.
+# TC:
+# SC:
+
+def numOfMinutes(self, n, headID, manager, informTime):
+        def dfs(i):
+            if manager[i] != -1:
+                informTime[i] += dfs(manager[i])
+                manager[i] = -1
+            return informTime[i]
+        return max(map(dfs, range(n)))
+
+########################################################################################################
+# Top-down DFS
+# TC:
+# SC:
+
+def numOfMinutes(self, n, headID, manager, informTime):
+        children = [[] for i in xrange(n)]
+        for i, m in enumerate(manager):
+            if m >= 0: children[m].append(i)
+
+        def dfs(i):
+            return max([dfs(j) for j in children[i]] or [0]) + informTime[i]
+        return dfs(headID)
