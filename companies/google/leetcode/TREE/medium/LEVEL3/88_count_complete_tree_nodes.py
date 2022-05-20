74d917a162015a76f38342373d642e8c9d1cc22e
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-20 13:36:46 -0700

    Create 88_count_complete_tree_nodes.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL3/88_count_complete_tree_nodes.py
@@ -0,0 +1,100 @@
+# https://leetcode.com/problems/count-complete-tree-nodes/
+# google 7
+'''
+222. Count Complete Tree Nodes
+
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+Example 2:
+
+Input: root = []
+Output: 0
+Example 3:
+
+Input: root = [1]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+'''
+
+#########################################################################################
+# Linear time: O(N)
+# SC: O(d) = O(log N), where d = tree depth
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        return 1 + self.countNodes(root.right) + self.countNodes(root.left) if root else 0
+   
+#########################################################################################
+# BINARY SEARCH
+# TC: O(d^2) = O( log N * log N)
+# SC: O(1)
+
+class Solution:
+    def compute_depth(self, node: TreeNode) -> int:
+        """
+        Return tree depth in O(d) time.
+        """
+        d = 0
+        while node.left:
+            node = node.left
+            d += 1
+        return d
+
+    def exists(self, idx: int, d: int, node: TreeNode) -> bool:
+        """
+        Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+        Return True if last level node idx exists. 
+        Binary search with O(d) complexity.
+        """
+        left, right = 0, 2**d - 1
+        for _ in range(d):
+            pivot = left + (right - left) // 2
+            if idx <= pivot:
+                node = node.left
+                right = pivot
+            else:
+                node = node.right
+                left = pivot + 1
+        return node is not None
+        
+    def countNodes(self, root: TreeNode) -> int:
+        # if the tree is empty
+        if not root:
+            return 0
+        
+        d = self.compute_depth(root)
+        # if the tree contains 1 node
+        if d == 0:
+            return 1
+        
+        # Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+        # Perform binary search to check how many nodes exist.
+        left, right = 1, 2**d - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if self.exists(pivot, d, root):
+                left = pivot + 1
+            else:
+                right = pivot - 1
+        
+        # The tree contains 2**d - 1 nodes on the first (d - 1) levels
+        # and left nodes on the last level.
+        return (2**d - 1) + left
+# Linear time
