b6054ebf4410fb800977b01e2c9be6cc0be48f48
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-20 13:08:56 -0700

    Update 82_print_binary_tree.py

--- companies/google/leetcode/TREE/medium/LEVEL3/82_print_binary_tree.py
+++ companies/google/leetcode/TREE/medium/LEVEL3/82_print_binary_tree.py
@@ -43,29 +43,24 @@ The depth of the tree will be in the range [1, 10].
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 ######################################################################################
 # RECURSION
 
-class Solution(object):
-    def printTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[str]]
-        """
+class Solution:
+    def printTree(self, root: Optional[TreeNode]) -> List[List[str]]:
         def get_height(node):
             return 0 if not node else 1 + max(get_height(node.left), get_height(node.right))
         
         def update_output(node, row, left, right):
             if not node:
                 return
-            mid = (left + right) / 2
+            mid = (left + right) // 2
             self.output[row][mid] = str(node.val)
             update_output(node.left, row + 1 , left, mid - 1)
             update_output(node.right, row + 1 , mid + 1, right)
             
         height = get_height(root)
         width = 2 ** height - 1
-        self.output = [[''] * width for i in xrange(height)]
+        self.output = [[''] * width for i in range(height)]
         update_output(node=root, row=0, left=0, right=width - 1)
         return self.output
