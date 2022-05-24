72d4a9f563de5fd39f7cefe2971e3186ceee69f5
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-24 10:00:32 -0700

    Create 96_flip_binary_tree_to_match_preorder_traversal.py

---
+++ companies/google/leetcode/TREE/medium/LEVEL4/96_flip_binary_tree_to_match_preorder_traversal.py
@@ -0,0 +1,88 @@
+# https://leetcode.com/problems/flip-binary-tree-to-match-preorder-traversal/
+'''
+971. Flip Binary Tree To Match Preorder Traversal
+
+You are given the root of a binary tree with n nodes, where each node is uniquely assigned a value from 1 to n. 
+You are also given a sequence of n values voyage, which is the desired pre-order traversal of the binary tree.
+
+Any node in the binary tree can be flipped by swapping its left and right subtrees. 
+For example, flipping node 1 will have the following effect:
+
+
+Flip the smallest number of nodes so that the pre-order traversal of the tree matches voyage.
+
+Return a list of the values of all flipped nodes. You may return the answer in any order. 
+If it is impossible to flip the nodes in the tree to make the pre-order traversal match voyage, return the list [-1].
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2], voyage = [2,1]
+Output: [-1]
+Explanation: It is impossible to flip the nodes such that the pre-order traversal matches voyage.
+Example 2:
+
+
+Input: root = [1,2,3], voyage = [1,3,2]
+Output: [1]
+Explanation: Flipping node 1 swaps nodes 2 and 3, so the pre-order traversal matches voyage.
+Example 3:
+
+
+Input: root = [1,2,3], voyage = [1,2,3]
+Output: []
+Explanation: The tree's pre-order traversal already matches voyage, so no nodes need to be flipped.
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+n == voyage.length
+1 <= n <= 100
+1 <= Node.val, voyage[i] <= n
+All the values in the tree are unique.
+All the values in voyage are unique.
+'''
+
+##############################################################################################################
+'''
+there was ambiguity in the question
+1.it was not mentioned that the input traversal of the tree was 'Level Order Traversal'
+2.it was difficult to think that inputs like [1, null,2,null,3,4,5,null,6] are possible in one go.
+'''
+
+##############################################################################################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def flipMatchVoyage(self, root: Optional[TreeNode], voyage: List[int]) -> List[int]:
+        self.flipped = []
+        self.i = 0
+
+        def dfs(node):
+            if node:
+                if node.val != voyage[self.i]:
+                    self.flipped = [-1]
+                    return
+                self.i += 1
+
+                if (self.i < len(voyage) and
+                        node.left and node.left.val != voyage[self.i]):
+                    self.flipped.append(node.val)
+                    dfs(node.right)
+                    dfs(node.left)
+                else:
+                    dfs(node.left)
+                    dfs(node.right)
+
+        dfs(root)
+        if self.flipped and self.flipped[0] == -1:
+            self.flipped = [-1]
+        return self.flipped
