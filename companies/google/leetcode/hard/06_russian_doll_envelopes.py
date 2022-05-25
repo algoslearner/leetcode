2423a3fa65fc20f9fdd094a74069053334e16831
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-25 11:02:02 -0700

    Create 06_russian_doll_envelopes.py

---
+++ companies/google/leetcode/hard/06_russian_doll_envelopes.py
@@ -0,0 +1,56 @@
+# https://leetcode.com/problems/russian-doll-envelopes/
+'''
+354. Russian Doll Envelopes
+
+You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width and the height of an envelope.
+
+One envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height.
+
+Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+
+Note: You cannot rotate an envelope.
+
+ 
+
+Example 1:
+
+Input: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+Output: 3
+Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+Example 2:
+
+Input: envelopes = [[1,1],[1,1],[1,1]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= envelopes.length <= 105
+envelopes[i].length == 2
+1 <= wi, hi <= 105
+'''
+
+##################################################################################################################
+# SORT + longest increasing subsequence (LIS)
+# TC: O(N log N)
+# SC: O(N)
+
+from bisect import bisect_left
+
+class Solution:
+    def maxEnvelopes(self, arr: List[List[int]]) -> int:
+        # sort increasing in first dimension and decreasing on second
+        arr.sort(key=lambda x: (x[0], -x[1]))
+
+        def lis(nums):
+            dp = []
+            for i in range(len(nums)):
+                idx = bisect_left(dp, nums[i])
+                if idx == len(dp):
+                    dp.append(nums[i])
+                else:
+                    dp[idx] = nums[i]
+            return len(dp)
+          
+        # extract the second dimension and run the LIS
+        return lis([i[1] for i in arr])
