ee3f38b20e6cf0ec89fc72896c1e558bf70f3c21
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-26 12:34:10 -0700

    Create 20_surrounded_regions.py

---
+++ companies/google/leetcode/matrix/medium/20_surrounded_regions.py
@@ -0,0 +1,117 @@
+# https://leetcode.com/problems/surrounded-regions/
+'''
+130. Surrounded Regions
+
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+ 
+
+Example 1:
+
+
+Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+Explanation: Surrounded regions should not be on the border, which means that any 'O' on the border of the board are not flipped to 'X'. 
+Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'. 
+Two cells are connected if they are adjacent cells connected horizontally or vertically.
+
+Example 2:
+
+Input: board = [["X"]]
+Output: [["X"]]
+ 
+
+Constraints:
+
+m == board.length
+n == board[i].length
+1 <= m, n <= 200
+board[i][j] is 'X' or 'O'.
+'''
+
+#############################################################################################################
+# DFS
+# The goal of this problem is to mark those captured cells
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board or not board[0]:
+            return
+
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+
+        # Step 1). retrieve all border cells
+        from itertools import product
+        borders = list(product(range(self.ROWS), [0, self.COLS-1])) \
+                + list(product([0, self.ROWS-1], range(self.COLS)))
+
+        # Step 2). mark the "escaped" cells, with any placeholder, e.g. 'E'
+        for row, col in borders:
+            self.DFS(board, row, col)
+
+        # Step 3). flip the captured cells ('O'->'X') and the escaped one ('E'->'O')
+        for r in range(self.ROWS):
+            for c in range(self.COLS):
+                if board[r][c] == 'O':   board[r][c] = 'X'  # captured
+                elif board[r][c] == 'E': board[r][c] = 'O'  # escaped
+
+
+    def DFS(self, board, row, col):
+        if board[row][col] != 'O':
+            return
+        board[row][col] = 'E'
+        if col < self.COLS-1: self.DFS(board, row, col+1)
+        if row < self.ROWS-1: self.DFS(board, row+1, col)
+        if col > 0: self.DFS(board, row, col-1)
+        if row > 0: self.DFS(board, row-1, col)
+          
+#############################################################################################################
+# BFS
+# TC: O(N)
+# SC: O(N)
+        
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board or not board[0]:
+            return
+
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+
+        # Step 1). retrieve all border cells
+        from itertools import product
+        borders = list(product(range(self.ROWS), [0, self.COLS-1])) \
+                + list(product([0, self.ROWS-1], range(self.COLS)))
+
+        # Step 2). mark the "escaped" cells, with any placeholder, e.g. 'E'
+        for row, col in borders:
+            #self.DFS(board, row, col)
+            self.BFS(board, row, col)
+
+        # Step 3). flip the captured cells ('O'->'X') and the escaped one ('E'->'O')
+        for r in range(self.ROWS):
+            for c in range(self.COLS):
+                if board[r][c] == 'O':   board[r][c] = 'X'  # captured
+                elif board[r][c] == 'E': board[r][c] = 'O'  # escaped
+
+
+    def BFS(self, board, row, col):
+        from collections import deque
+        queue = deque([(row, col)])
+        while queue:
+            (row, col) = queue.popleft()
+            if board[row][col] != 'O':
+                continue
+            # mark this cell as escaped
+            board[row][col] = 'E'
+            # check its neighbor cells
+            if col < self.COLS-1: queue.append((row, col+1))
+            if row < self.ROWS-1: queue.append((row+1, col))
+            if col > 0: queue.append((row, col-1))
+            if row > 0: queue.append((row-1, col))
+        
