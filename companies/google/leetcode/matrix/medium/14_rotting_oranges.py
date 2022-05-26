f137ef335dde5b0ea6d46a5418b2eb1a9df688b9
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-26 09:55:48 -0700

    Create 14_rotting_oranges.py

---
+++ companies/google/leetcode/matrix/medium/14_rotting_oranges.py
@@ -0,0 +1,128 @@
+# https://leetcode.com/problems/rotting-oranges/
+# amazon 40, linkedin 3
+'''
+994. Rotting Oranges
+
+You are given an m x n grid where each cell can have one of three values:
+
+0 representing an empty cell,
+1 representing a fresh orange, or
+2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+Example 2:
+
+Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+Example 3:
+
+Input: grid = [[0,2]]
+Output: 0
+Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 10
+grid[i][j] is 0, 1, or 2.
+'''
+
+########################################################################################
+# BFS
+# TC: O(N)
+# SC: O(N)
+
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue = deque()
+
+        # Step 1). build the initial set of rotten oranges
+        fresh_oranges = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh_oranges += 1
+
+        # Mark the round / level, _i.e_ the ticker of timestamp
+        queue.append((-1, -1))
+
+        # Step 2). start the rotting process via BFS
+        minutes_elapsed = -1
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        while queue:
+            row, col = queue.popleft()
+            if row == -1:
+                # We finish one round of processing
+                minutes_elapsed += 1
+                if queue:  # to avoid the endless loop
+                    queue.append((-1, -1))
+            else:
+                # this is a rotten orange
+                # then it would contaminate its neighbors
+                for d in directions:
+                    neighbor_row, neighbor_col = row + d[0], col + d[1]
+                    if ROWS > neighbor_row >= 0 and COLS > neighbor_col >= 0:
+                        if grid[neighbor_row][neighbor_col] == 1:
+                            # this orange would be contaminated
+                            grid[neighbor_row][neighbor_col] = 2
+                            fresh_oranges -= 1
+                            # this orange would then contaminate other oranges
+                            queue.append((neighbor_row, neighbor_col))
+
+        # return elapsed minutes if no fresh orange left
+        return minutes_elapsed if fresh_oranges == 0 else -1
+      
+      
+#######################################################################################################
+# in-place bfs
+# TC: O(N^2)
+# SC: O(1)
+
+class Solution(object):
+    def orangesRotting(self, grid):
+        ROWS, COLS = len(grid), len(grid[0])
+
+        # run the rotting process, by marking the rotten oranges with the timestamp
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        def runRottingProcess(timestamp):
+            # flag to indicate if the rotting process should be continued
+            to_be_continued = False
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if grid[row][col] == timestamp:
+                        # current contaminated cell
+                        for d in directions:
+                            n_row, n_col = row + d[0], col + d[1]
+                            if ROWS > n_row >= 0 and COLS > n_col >= 0:
+                                if grid[n_row][n_col] == 1:
+                                    # this fresh orange would be contaminated next
+                                    grid[n_row][n_col] = timestamp + 1
+                                    to_be_continued = True
+            return to_be_continued
+
+        timestamp = 2
+        while runRottingProcess(timestamp):
+            timestamp += 1
+        # end of process, to check if there are still fresh oranges left
+        for row in grid:
+            for cell in row:
+                if cell == 1:  # still got a fresh orange left
+                    return -1
+        # return elapsed minutes if no fresh orange left
+        return timestamp - 2
