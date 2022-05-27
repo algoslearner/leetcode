c45408f9ac4b2fcea50b620a1d01f6e2f7e9d0f6
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-05-27 15:13:20 -0700

    Create 03_greatest_number_in_matrix.py

---
+++ companies/google/discuss/onsite/03_greatest_number_in_matrix.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/discuss/interview-question/2077984/google-onsite-sde
+'''
+Given NxN matrix with integer numbers.
+Find the greatest number for every 3x3 matrix.
+
+update example
+
+example.1
+[[1,2,3],
+[4,5,6],
+[7,8,9]]
+
+ans:
+[9]
+
+example.2
+[[1,2,3,6],
+[5,2,4,1],
+[3,4,2,3],
+[8,2,1,4]]
+
+ans:
+[[5,6],
+[8,4]]
+'''
