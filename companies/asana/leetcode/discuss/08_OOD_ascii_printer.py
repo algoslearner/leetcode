2fdd9bb50e8cf2f63679dc6431debece8f1c6b3d
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-01 11:21:11 -0700

    Create 08_OOD_ascii_printer.py

---
+++ companies/asana/leetcode/discuss/08_OOD_ascii_printer.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/discuss/interview-question/866230/Asana-or-Software-Engineer-or-Reject
+'''
+Program an ASCII printer. I think this was OOP related question which I thought is as simple as ''.join(str(ord(c)) for c in s)
+I searched this question online but couldn't find an answer. I would love to know what the potential answer looks like.
+'''
+
+# Do you know what was ascii printer question? something like this? http://memex.cc/Technical_interview#print-out-an-ascii-art-piece
