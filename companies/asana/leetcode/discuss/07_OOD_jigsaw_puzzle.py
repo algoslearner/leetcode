9cba660fe1bbb9ab4542e0196e86fcea17d7bceb
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-01 11:19:24 -0700

    Update 07_OOD_jigsaw_puzzle.py

--- companies/asana/leetcode/discuss/07_OOD_jigsaw_puzzle.py
+++ companies/asana/leetcode/discuss/07_OOD_jigsaw_puzzle.py
@@ -1,3 +1,11 @@
-# More like a design question. Given a Piece class and a Side class, design a method solve(pieces[]) which returns a solved puzzle. I had to design the data structure for solved puzzle and how I would write the solve method.
+# More like a design question. 
+# Given a Piece class and a Side class, design a method solve(pieces[]) which returns a solved puzzle. 
+# I had to design the data structure for solved puzzle and how I would write the solve method.
+# you don't need backtracking, since each piece is unique
+
+####################################################################################
+# OOP design of a jigsaw puzzle. 
+# Imagine you want to solve a rectangular jigsaw puzzle. What classes and relationships do you need? Design an algo for solving the actual puzzle
+
 
 # SOLUTION: https://massivetechinterview.blogspot.com/2015/07/implement-jigsaw-puzzle-kodeknight.html
