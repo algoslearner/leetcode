58bb7221404a588b6c35f8bf32a891ad3b1169e4
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-01 08:05:54 -0700

    Create 11_min_stack.py

---
+++ companies/fb/leetcode/DESIGN/easy/11_min_stack.py
@@ -0,0 +1,110 @@
+# https://leetcode.com/problems/min-stack/
+'''
+155. Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.
+'''
+
+##########################################################################################################
+# two stacks
+# TC: O(1)
+# SC: O(N)
+
+class MinStack:
+
+    def __init__(self):
+        self.output = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.output.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.min_stack[-1] == self.output[-1]:
+            self.min_stack.pop()
+        self.output.pop()
+
+    def top(self) -> int:
+        return self.output[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+##########################################################################################################
+# stack of value and minimum pairs
+# TC: O(1)
+# SC: O(N)
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        
+        # If the stack is empty, then the min value
+        # must just be the first value we add
+        if not self.stack:
+            self.stack.append((x, x))
+            return
+
+        current_min = self.stack[-1][1]
+        self.stack.append((x, min(x, current_min)))
+        
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
