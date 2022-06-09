1a142b608f593e21255dcd7143577e0f78e87281
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-09 09:19:27 -0700

    Create 02_find_the_duplicate_number.py

---
+++ companies/google/leetcode/LINKEDLIST/medium/02_find_the_duplicate_number.py
@@ -0,0 +1,94 @@
+# https://leetcode.com/problems/find-the-duplicate-number/
+'''
+287. Find the Duplicate Number
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+
+Follow up:
+
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem in linear runtime complexity?
+'''
+
+#####################################################################################################################
+# sort
+# TC: O(n log n)
+# SC: O(1)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
+              
+#####################################################################################################################
+# set
+# TC: O(n)
+# SC: O(n)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+            
+#####################################################################################################################
+# Floyd's Tortoise and Hare (Cycle Detection)
+# TC: O(n)
+# SC: O(1)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+        
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        # find entrance to cycle
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return fast
+
+#####################################################################################################################
+# given array as hashmap
+# TC: O(n)
+# SC: O(1)
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        while nums[0] != nums[nums[0]]:
+            nums[nums[0]], nums[0] = nums[0], nums[nums[0]]
+        return nums[0]
