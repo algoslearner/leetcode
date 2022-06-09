0d53feeb49fe082d5b7258ab4ceedd861a9cd1c8
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-09 15:14:38 -0700

    Update 19_valid_sudoku.py

--- companies/google/leetcode/matrix/medium/19_valid_sudoku.py
+++ companies/google/leetcode/matrix/medium/19_valid_sudoku.py
@@ -90,3 +90,42 @@ class Solution:
                 boxes[idx].add(val)
 
         return True
+
+####################################################################################################
+# bitmasking
+# TC: O(N^2)
+# SC: O(N)
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        N = 9
+        # Use binary number to check previous occurrence
+        rows = [0] * N
+        cols = [0] * N
+        boxes = [0] * N
+
+        for r in range(N):
+            for c in range(N):
+                # Check if the position is filled with number
+                if board[r][c] == ".":
+                    continue
+
+                pos = int(board[r][c]) - 1
+
+                # Check the row
+                if rows[r] & (1 << pos):
+                    return False
+                rows[r] |= (1 << pos)
+
+                # Check the column
+                if cols[c] & (1 << pos):
+                    return False
+                cols[c] |= (1 << pos)
+
+                # Check the box
+                idx = (r // 3) * 3 + c // 3
+                if boxes[idx] & (1 << pos):
+                    return False
+                boxes[idx] |= (1 << pos)
+
+        return True
