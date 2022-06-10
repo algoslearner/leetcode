b395af85b80f8300da0c364e3b676daf9beefbd8
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-10 08:24:40 -0700

    Update 04_binary_tree_level_order_traversal.py

--- companies/google/leetcode/TREE/easy/04_binary_tree_level_order_traversal.py
+++ companies/google/leetcode/TREE/easy/04_binary_tree_level_order_traversal.py
@@ -60,20 +60,41 @@ class Solution:
 ######################################################################################
 # ITERATION using queue
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        
+        result = []
+        q = deque()
+        q.append(root)
+        while q:
+            levelsize = len(q)
+            curr_level = []
+            for _ in range(levelsize):
+                node = q.popleft()
+                curr_level.append(node.val)
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            result.append(curr_level)
+        return result
+            
+        
+    def levelOrder2(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
         
         q = deque([(root, 0)])
         level_order = defaultdict(list)
-        
         while q:
             node, level = q.popleft()
             level_order[level].append(node.val)
-            if node.left:
-                q.append((node.left, level+1))
-            if node.right:
-                q.append((node.right, level+1))
-
+            if node.left: q.append((node.left, level+1))
+            if node.right: q.append((node.right, level+1))
         return level_order.values()
