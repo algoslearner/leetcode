b36e0a5373f207cd1048c09c0094e6eaef03f5af
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-15 12:11:51 -0700

    Update 06_house_thief.py

--- grokking/dp/01_fibonacci_numbers/06_house_thief.py
+++ grokking/dp/01_fibonacci_numbers/06_house_thief.py
@@ -110,3 +110,29 @@ def main():
 
 
 main()
+
+
+#############################################################################################################################
+# bottom up dp + memory optimization
+# TC: O(n)
+# SC: O(1)
+
+def find_max_steal(wealth):
+  n = len(wealth)
+  if n == 0:
+    return 0
+
+  n1, n2 = 0, wealth[0]
+  for i in range(1, n):
+    n1, n2 = n2, max(n1 + wealth[i], n2)
+
+  return n2
+
+
+def main():
+  print(find_max_steal([2, 5, 1, 3, 6, 2, 4]))
+  print(find_max_steal([2, 10, 14, 8, 1]))
+
+
+main()
+
