120aabaddbe1a4cbfb4c3b163fdb9b1d8d79f86f
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 14:40:35 -0700

    Create 76_dice_throw.py

---
+++ companies/amazon/OA/76_dice_throw.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/discuss/interview-question/445291/Amazon-or-OA-2019
+'''
+Recently I gave Amazon assessment for SDE Intern and the test was of 90 minute. There were two sections A and B.
+
+Section A consist of two Coding Questions -->
+First coding question was "find the next maximum permutation of any number" and
+Second coding question was "Dice Throw" problem of DP which is available on geeksforgeeks.
+Section B consist of 28 MCQs questions purely on Data Structures and on C++ and C based programming.
+
+
+Amcat?
+
+Dice Throw
+
+https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/?
+
+find the next maximum permutation of any number
+
+https://leetcode.com/problems/next-permutation/?
+
+'''
