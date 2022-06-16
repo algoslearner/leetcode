9da2cbf48e0fbdb79b225cf405e4f3ed7fe7ff38
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 14:38:04 -0700

    Create 75_temple_offerings.py

---
+++ companies/amazon/OA/75_temple_offerings.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/discuss/interview-question/446075/Amazon-or-Phone-or-Candy
+'''
+Position: SDE L4 position at Amazon AWS.
+Location: Seattle
+
+1hr Phone screen on Amazon Chime and asked this exact question: https://www.geeksforgeeks.org/temple-offerings/
+
+LC version: https://leetcode.com/problems/candy/
+
+Small amount of time left over to discuss my projects and and questions I had for them.
+'''
