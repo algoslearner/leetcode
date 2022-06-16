a2eb8068258543e3f1df01259e4334f9e7e88ba4
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 14:25:59 -0700

    Create 73_course_schedule_ii.py

---
+++ companies/amazon/OA/73_course_schedule_ii.py
@@ -0,0 +1,14 @@
+#
+'''
+Duration:1 hour
+
+Platform: Amazon live code(no compilation required, something similar to Google doc but there was option to choose your language so the syntax highlight was available.)
+
+Behavioral: Describe a challenging situation(non-tech) that you have faced and how did you resolve that?
+
+Topological Sort
+https://leetcode.com/problems/course-schedule-ii/description/
+
+Result:
+Cleared the phone screen but the position is filled.
+'''
