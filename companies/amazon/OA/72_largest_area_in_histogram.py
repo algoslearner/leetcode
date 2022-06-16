b6fe1145305714afd1909628b7410a29dbe43fbb
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 13:46:57 -0700

    Create 72_largest_area_in_histogram.py

---
+++ companies/amazon/OA/72_largest_area_in_histogram.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/discuss/interview-question/1756806/Amazon-or-OA
+'''
+Q1 : Largest Area in Histogram with one extra constraint in orginal question you included equal heights bars in the area calculation here you had to avoid that
+
+Q2 : Similar to LC 1094 with large constraints expected O(N) solution
+'''
