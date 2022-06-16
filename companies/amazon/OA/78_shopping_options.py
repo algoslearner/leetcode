42ac00737afd1069bfa083a52bbc543f077a6c05
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 14:49:10 -0700

    Update 78_shopping_options.py

--- companies/amazon/OA/78_shopping_options.py
+++ companies/amazon/OA/78_shopping_options.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/discuss/interview-question/1051370/Amazon-OA-Assessment
+# check this post : https://leetcode.com/discuss/interview-question/1023609/amazon-oa-shopping-options
 '''
 I was asked these questions for Amazon online assessment (04-FEB-2021):
 Shopping Options
