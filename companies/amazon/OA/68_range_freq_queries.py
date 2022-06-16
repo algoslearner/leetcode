0605f919c9faba1c422f62725bdfe604334ed847
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 13:38:04 -0700

    Create 68_range_freq_queries.py

---
+++ companies/amazon/OA/68_range_freq_queries.py
@@ -0,0 +1,58 @@
+# https://leetcode.com/discuss/interview-question/1856295/Amazon-Question
+'''
+Given an array of integers -a- and a set of queries of the form [ l, r, x ], your task is to calculate the number of occurrences of the number x in the inclusive subarray a[l…r] (0-based), for each query, Return the sum of the answers for all queries as the result.
+
+Example
+
+for a=[1,2,1,3,1,2,1] and
+
+arr = [ [1,3,3],
+[0,4,1],
+[2,5,2],
+[5,6,1]
+]
+
+The output should be solution(queries) = 6.
+
+-The answer to the first query is 1 : the number 3 appears 1 time in the subarray [2,1,3];
+
+-The answer to the second query is 3: the number 1 appears 3 times in the subarray [1,2,1,3,1]:
+
+-The answer to the third query is 1: the number 2 appears 1 time in the subarray [1,3,1,2];
+
+-The answer to the fourth query is 1 : the number 1 appears 1 time in the subarray [2,1] .
+
+-So the answer is 1 + 3 + 1 + 1 = 6.
+
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[input] array.integer a
+
+An array of integers.
+
+Guaranteed constraints
+
+1 <= a.length <= 10^4
+
+[input] array.array.integer queries
+
+An array of queries, where queries [ i ] contains three numbers : l , r and x
+
+Guaranteed constraints:
+3<= queries.length <= 10^5
+queries[ i ].length = 3,
+0<= queries[ i ] [ 0 ] <= queries [ i ] [ 1] < a.length
+1<= queries [ i ] [ 2 ] <= 100.
+
+[output] integer
+
+The sum of the answers to all queries
+
+Int solution (int[] a , int [] [] queries ){
+
+// solution here
+
+}
+'''
