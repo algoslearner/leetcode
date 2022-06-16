cfab1f89c597c96acb077d48777e7e1c57d5c9d0
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 14:36:29 -0700

    Create 74_rotting_oranges.py

---
+++ companies/amazon/OA/74_rotting_oranges.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/discuss/interview-question/525680/Amazon-Online-Assessment-US
+'''
+Hey Leetcode community,
+
+Took my Amazon online assessment yesterday, the following questions were asked
+
+https://leetcode.com/problems/rotting-oranges/solution/
+https://leetcode.com/discuss/interview-question/460127/
+
+Thanks and good luck.
+'''
