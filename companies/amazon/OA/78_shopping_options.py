ce0284dc3dde30e6b35d84b7cd3c6f979f3ff455
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 14:50:51 -0700

    Update 78_shopping_options.py

--- companies/amazon/OA/78_shopping_options.py
+++ companies/amazon/OA/78_shopping_options.py
@@ -5,7 +5,43 @@ I was asked these questions for Amazon online assessment (04-FEB-2021):
 Shopping Options
 https://leetcode.com/problems/robot-bounded-in-circle/
 
-Platform: Hackerrank
-Level : Experienced
-Location : U.K
+
+These were the exact questions asked, with 2 free-form questions to explain the solution as well as the runtime.
+https://leetcode.com/discuss/interview-question/1023609/amazon-oa-shopping-options
+https://leetcode.com/problems/robot-bounded-in-circle/
+'''
+
+
+###################################################################
+'''
+Disappeared shopping options question, retrieved from google cache
+
+A customer wants to buy a pair of jeans, a pair of shoes, a skirt, and a top but has a limited budget in dollars. Given different pricing options for each product, determine how many options our customer has to buy 1 of each product. You cannot spend more money than the budgeted amount.
+
+Example
+priceOfJeans = [2, 3]
+priceOfShoes = [4]
+priceOfSkirts = [2, 3]
+priceOfTops = [1, 2]
+budgeted = 10
+
+The customer must buy shoes for 4 dollars since there is only one option. This leaves 6 dollars to spend on the other 3 items. Combinations of prices paid for jeans, skirts, and tops respectively that add up to 6 dollars or less are [2, 2, 2], [2, 2, 1], [3, 2, 1], [2, 3, 1]. There are 4 ways the customer can purchase all 4 items.
+
+Function Description
+
+Complete the getNumberOfOptions function in the editor below. The function must return an integer which represents the number of options present to buy the four items.
+
+getNumberOfOptions has 5 parameters:
+int[] priceOfJeans: An integer array, which contains the prices of the pairs of jeans available.
+int[] priceOfShoes: An integer array, which contains the prices of the pairs of shoes available.
+int[] priceOfSkirts: An integer array, which contains the prices of the skirts available.
+int[] priceOfTops: An integer array, which contains the prices of the tops available.
+int dollars: the total number of dollars available to shop with.
+
+Constraints
+
+1 ≤ a, b, c, d ≤ 103
+1 ≤ dollars ≤ 109
+1 ≤ price of each item ≤ 109
+Note: a, b, c and d are the sizes of the four price arrays
 '''
