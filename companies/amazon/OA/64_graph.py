6122537ac2ebf773a5c406eb45671506006a1a8b
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-16 11:55:36 -0700

    Create 64_graph.py

---
+++ companies/amazon/OA/64_graph.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/discuss/interview-question/2007284/Graph-Amazon
+
+# 1. GRAPH
+'''
+You are given a tree with V vertices numbered from 1 to N.
+The ith edge connects Vertex xi and Vertex yi bidirectionally. You
+have to divide this tree into three connected components by cutting
+any two edges of the tree. Let the three components be C1,
+C2 and C3. Let X1, X2 and X3 be the XOR of all the vertices of the
+components C1, C2 and C3 respectively.
+Task
+Minimize the difference between the maximum and minimum xor
+values of the components. In short, you have to minimize the value
+of max(X1, X2 ,X3)-min(X1, x2,X3)
+'''
+
+# 2. Problem was similar to leetcode 828. Count Unique Characters of All Substrings of a Given String.. 
+# It was not exact same little bit modification was there, but similar kind of approach will be used to solve this problem .
