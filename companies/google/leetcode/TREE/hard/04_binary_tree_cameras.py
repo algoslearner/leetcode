c1a2f25c87328e5dbd8c7f53496823e581d09e31
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-17 09:04:22 -0700

    Update 04_binary_tree_cameras.py

--- companies/google/leetcode/TREE/hard/04_binary_tree_cameras.py
+++ companies/google/leetcode/TREE/hard/04_binary_tree_cameras.py
@@ -57,3 +57,26 @@ class Solution:
             return dp0, dp1, dp2
         
         return min(solve(root)[1:])
+       
+##############################################################################################################################
+# greedy
+# TC: O(N)
+# SC: O(H)
+
+class Solution(object):
+    def minCameraCover(self, root):
+        self.ans = 0
+        covered = {None}
+
+        def dfs(node, par = None):
+            if node:
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+                if (par is None and node not in covered or
+                        node.left not in covered or node.right not in covered):
+                    self.ans += 1
+                    covered.update({node, par, node.left, node.right})
+
+        dfs(root)
+        return self.ans
