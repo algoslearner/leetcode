eb13ef0448214519d4e68129e3efff667a78db4b
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-06-23 08:26:05 -0700

    Update 06_items_in_containers.py

--- companies/amazon/OA/06_items_in_containers.py
+++ companies/amazon/OA/06_items_in_containers.py
@@ -147,4 +147,76 @@ public List<Integer> numberOfItems(String s, List<Integer> startIndices, List<In
 
 ##########################################################################################################
 # Similar leetcode question: problem2 : https://leetcode.com/problems/plates-between-candles/
+'''
+2055. Plates Between Candles
+
+There is a long table with a line of plates and candles arranged on top of it. You are given a 0-indexed string s consisting of characters '*' and '|' only, where a '*' represents a plate and a '|' represents a candle.
+
+You are also given a 0-indexed 2D integer array queries where queries[i] = [lefti, righti] denotes the substring s[lefti...righti] (inclusive). For each query, you need to find the number of plates between candles that are in the substring. A plate is considered between candles if there is at least one candle to its left and at least one candle to its right in the substring.
+
+For example, s = "||**||**|*", and a query [3, 8] denotes the substring "*||**|". The number of plates between candles in this substring is 2, as each of the two plates has at least one candle in the substring to its left and right.
+Return an integer array answer where answer[i] is the answer to the ith query.
+
+ 
+
+Example 1:
+
+ex-1
+Input: s = "**|**|***|", queries = [[2,5],[5,9]]
+Output: [2,3]
+Explanation:
+- queries[0] has two plates between candles.
+- queries[1] has three plates between candles.
+Example 2:
+
+ex-2
+Input: s = "***|**|*****|**||**|*", queries = [[1,17],[4,5],[14,17],[5,11],[15,16]]
+Output: [9,0,0,0,0]
+Explanation:
+- queries[0] has nine plates between candles.
+- The other queries have zero plates between candles.
+ 
+
+Constraints:
+
+3 <= s.length <= 105
+s consists of '*' and '|' characters.
+1 <= queries.length <= 105
+queries[i].length == 2
+0 <= lefti <= righti < s.length
+'''
+
+#########################################################################################################################
+# binary search + hashmap
+# TC: O(N + Q log N)
+# SC: O(N)
+
+import bisect
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        # count the number of plates:
+        count = 0
+        hashmap = collections.defaultdict()
+        pos = []
+        for i, c in enumerate(s):
+            if c == "*":
+                count += 1
+            if c == '|':
+                hashmap[i] = count
+                pos.append(i)
+                
+        result = [0]*len(queries)
+        # retrieve by queries
+        for i, (l, r) in enumerate(queries):
+            # use binary search of the left and right commands
+            idx_l = bisect.bisect_left(pos, l)
+            idx_r = bisect.bisect_right(pos, r) - 1
+            if (idx_l >= idx_r) or (idx_l < 0) or (idx_r >= len(pos)):
+                continue
+            l = pos[idx_l]
+            r = pos[idx_r]
+            result[i] = hashmap[r] - hashmap[l]
+        # time: O(N + Q*logN)
+        # space: O(N)
+        return result
 
