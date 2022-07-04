3d8f0e44c87c39749e8fd0553b05974155b25f8c
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-04 11:43:59 -0700

    Update 04_paint_house.py

--- companies/linkedin/leetcode/medium/04_paint_house.py
+++ companies/linkedin/leetcode/medium/04_paint_house.py
@@ -36,6 +36,33 @@ costs[i].length == 3
 '''
 
 ###############################################################################################
+# Brute force
+# TC: O(2^n)
+# SC: O(n)
+
+def minCost(self, costs):
+    """
+    :type costs: List[List[int]]
+    :rtype: int
+    """
+
+    def paint_cost(n, color):
+        total_cost = costs[n][color]
+        if n == len(costs) - 1:
+            pass
+        elif color == 0: # Red
+            total_cost += min(paint_cost(n + 1, 1), paint_cost(n + 1, 2))
+        elif color == 1: # Green
+            total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 2))
+        else: # Blue
+            total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 1))
+        return total_cost
+
+    if costs == []:
+        return 0
+    return min(paint_cost(0, 0), paint_cost(0, 1), paint_cost(0, 2))
+
+###############################################################################################
 # DP
 # TC: O(N)
 # SC: O(1)
