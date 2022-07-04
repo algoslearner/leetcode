b1ca89d3b4826a0efc5d71afc96097fe00ba19fd
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-04 07:46:20 -0700

    Create 09_find_smallest_divisor_given_thresold.py

---
+++ companies/amazon/leetcode/medium/09_find_smallest_divisor_given_thresold.py
@@ -0,0 +1,50 @@
+# https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+'''
+1283. Find the Smallest Divisor Given a Threshold
+
+Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. 
+Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
+
+Each result of the division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
+
+The test cases are generated so that there will be an answer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,5,9], threshold = 6
+Output: 5
+Explanation: We can get a sum to 17 (1+2+5+9) if the divisor is 1. 
+If the divisor is 4 we can get a sum of 7 (1+1+2+3) and if the divisor is 5 the sum will be 5 (1+1+1+2). 
+Example 2:
+
+Input: nums = [44,22,33,11,1], threshold = 5
+Output: 44
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+1 <= nums[i] <= 106
+nums.length <= threshold <= 106
+'''
+
+############################################################################################################################################################
+# binary search
+# TC:
+# SC:
+
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        def condition(divisor) -> bool:
+            return sum((num - 1) // divisor + 1 for num in nums) <= threshold
+        
+        left, right = 1, max(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if condition(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
