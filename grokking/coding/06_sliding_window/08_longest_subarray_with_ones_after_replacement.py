87a2da6d45b1e1c33543b2fd614b48509c28b050
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-07 04:52:06 -0700

    Update 08_longest_subarray_with_ones_after_replacement.py

--- grokking/coding/06_sliding_window/08_longest_subarray_with_ones_after_replacement.py
+++ grokking/coding/06_sliding_window/08_longest_subarray_with_ones_after_replacement.py
@@ -19,21 +19,24 @@ Explanation: Replace the '0' at index 6, 9, and 10 to have the longest contiguou
 # TC: O(N)
 # SC: O(1)
 
-def length_of_longest_substring(arr, k):
-  maxlength = 0
-  start = 0
-  freqmap = {}
-  
-  for end in range(len(arr)):
-    right_char = arr[end]
-    freqmap[right_char] = freqmap.get(right_char, 0) + 1
-    
-    window_size = end - start + 1
-    if window_size - freqmap[1] > k:
-      left_char = arr[start]
-      if left_char == 1:
-        freqmap[1] -= 1
-      start ++ 1
-    
-    maxlength = max(maxlength, end - start + 1)
-  return maxlength
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        maxlength = 0
+        start = 0
+        max_ones_count = 0
+        
+        for end in range(len(nums)):
+            right_char = nums[end]
+            if right_char == 1:
+                max_ones_count += 1
+            
+            window_size = end - start + 1
+            if window_size - max_ones_count > k:
+                left_char = nums[start]
+                if left_char == 1:
+                    max_ones_count -= 1
+                start += 1
+            
+            maxlength = max(maxlength, end - start + 1)
+        return maxlength
+            
