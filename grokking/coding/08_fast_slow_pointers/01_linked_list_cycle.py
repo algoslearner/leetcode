6370df43cda792d661a0efcc619ab059f06a4279
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-10 14:12:24 -0700

    Update 01_linked_list_cycle.py

--- grokking/coding/08_fast_slow_pointers/01_linked_list_cycle.py
+++ grokking/coding/08_fast_slow_pointers/01_linked_list_cycle.py
@@ -21,3 +21,30 @@ def hasCycle(head):
     
   return False
     
+
+######################################################################################## 
+  # Problem 1: Given the head of a LinkedList with a cycle, find the length of the cycle.
+  # TC: O(N)
+  # SC: O(1)
+########################################################################################
+
+def find_cycle_length(head):
+  slow, fast = head, head
+  while fast is not None and fast.next is not None:
+    fast = fast.next.next
+    slow = slow.next
+    if slow == fast:  # found the cycle
+      return calculate_cycle_length(slow)
+
+  return 0
+
+
+def calculate_cycle_length(slow):
+  current = slow
+  cycle_length = 0
+  while True:
+    current = current.next
+    cycle_length += 1
+    if current == slow:
+      break
+  return cycle_length
