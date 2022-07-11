e943b115e76fd6fd98c1331740188e1d13993c51
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-11 13:30:40 -0700

    Create 01_subsets.py

---
+++ grokking/coding/11_subsets/01_subsets.py
@@ -0,0 +1,32 @@
+#
+'''
+Given a set with distinct elements, find all of its distinct subsets.
+
+Example 1:
+
+Input: [1, 3]
+Output: [], [1], [3], [1,3]
+Example 2:
+
+Input: [1, 5, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
+'''
+
+##############################################################################################################################
+# TC: O(N * 2^N)
+# SC: O(N * 2^N)
+
+def find_subsets(nums):
+  subsets = []
+  # start by adding the empty subset
+  subsets.append([])
+  for currentNumber in nums:
+    # we will take all existing subsets and insert the current number in them to create new subsets
+    n = len(subsets)
+    for i in range(n):
+      # create a new subset from the existing subset and insert the current element to it
+      set1 = list(subsets[i])
+      set1.append(currentNumber)
+      subsets.append(set1)
+
+  return subsets
