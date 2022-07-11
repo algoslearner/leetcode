dbd86dfc2e45719af1b9c27b72f4ea2a83ba75c4
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-10 22:12:10 -0700

    Create 04_conflicting_appointments.py

---
+++ grokking/coding/09_merge_intervals/04_conflicting_appointments.py
@@ -0,0 +1,36 @@
+#
+'''
+Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+
+Example 1:
+
+Appointments: [[1,4], [2,5], [7,9]]
+Output: false
+Explanation: Since [1,4] and [2,5] overlap, a person cannot attend both of these appointments.
+Example 2:
+
+Appointments: [[6,7], [2,4], [8,12]]
+Output: true
+Explanation: None of the appointments overlap, therefore a person can attend all of them.
+Example 3:
+
+Appointments: [[4,5], [2,3], [3,6]]
+Output: false
+Explanation: Since [4,5] and [3,6] overlap, a person cannot attend both of these appointments.
+'''
+
+###################################################################################################################################
+# sorting
+# TC: O(N log N)
+# SC: O(N)
+
+def can_attend_all(intervals) -> bool:
+  intervals.sort(key = lambda x : x[0])
+  
+  start, end = 0, 1
+  for i in range(1, len(intervals)):
+    if intervals[i][start] < intervals[i - 1][end]:
+      return False
+    
+  return True
+
