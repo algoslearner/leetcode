c286da590c7d4d67ff037cb9f55064e8e9058f0b
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-10 21:59:35 -0700

    Create 03_intervals_list_intersection.py

---
+++ grokking/coding/09_merge_intervals/03_intervals_list_intersection.py
@@ -0,0 +1,65 @@
+# https://leetcode.com/problems/interval-list-intersections/
+'''
+986. Interval List Intersections
+
+You are given two lists of closed intervals, firstList and secondList, 
+where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+
+The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. 
+For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+ 
+
+Example 1:
+
+
+Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Example 2:
+
+Input: firstList = [[1,3],[5,9]], secondList = []
+Output: []
+ 
+
+Constraints:
+
+0 <= firstList.length, secondList.length <= 1000
+firstList.length + secondList.length >= 1
+0 <= starti < endi <= 109
+endi < starti+1
+0 <= startj < endj <= 109
+endj < startj+1
+'''
+
+################################################################################################################################################
+# two pointers
+# TC: O(n + m)
+# SC: O(n + m), for output
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        output = []
+        i = j = 0
+        A = firstList
+        B = secondList
+
+        while i < len(A) and j < len(B):
+            # Let's check if A[i] intersects B[j].
+            # lo - the startpoint of the intersection
+            # hi - the endpoint of the intersection
+            lo = max(A[i][0], B[j][0])
+            hi = min(A[i][1], B[j][1])
+            if lo <= hi:
+                output.append([lo, hi])
+
+            # Remove the interval with the smallest endpoint
+            if A[i][1] < B[j][1]:
+                i += 1
+            else:
+                j += 1
+
+        return output
