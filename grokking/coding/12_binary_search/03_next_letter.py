73ae75d6d0a6776dba6aa9325ca4f8b2d61c8d20
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-12 15:00:01 -0700

    Create 03_next_letter.py

---
+++ grokking/coding/12_binary_search/03_next_letter.py
@@ -0,0 +1,59 @@
+#
+'''
+Given an array of lowercase letters sorted in ascending order, find the smallest letter in the given array greater than a given ‘key’.
+
+Assume the given array is a circular list, which means that the last letter is assumed to be connected with the first letter. This also means that the smallest letter in the given array is greater than the last letter of the array and is also the first letter of the array.
+
+Write a function to return the next letter of the given ‘key’.
+
+Example 1:
+
+Input: ['a', 'c', 'f', 'h'], key = 'f'
+Output: 'h'
+Explanation: The smallest letter greater than 'f' is 'h' in the given array.
+Example 2:
+
+Input: ['a', 'c', 'f', 'h'], key = 'b'
+Output: 'c'
+Explanation: The smallest letter greater than 'b' is 'c'.
+Example 3:
+
+Input: ['a', 'c', 'f', 'h'], key = 'm'
+Output: 'a'
+Explanation: As the array is assumed to be circular, the smallest letter greater than 'm' is 'a'.
+'''
+
+#########################################################################################################################################
+# binary search
+# TC: O(log n)
+# SC: O(1)
+
+def search_next_letter(letters, key):
+  n = len(letters)
+
+  start, end = 0, n - 1
+  while start <= end:
+    mid = start + (end - start) // 2
+    if key < letters[mid]:
+      end = mid - 1
+    else: # key >= letters[mid]:
+      start = mid + 1
+
+  # since the loop is running until 'start <= end', so at the end of the while loop, 'start == end+1'
+  return letters[start % n]
+
+
+def main():
+  print(search_next_letter(['a', 'c', 'f', 'h'], 'f'))
+  print(search_next_letter(['a', 'c', 'f', 'h'], 'b'))
+  print(search_next_letter(['a', 'c', 'f', 'h'], 'm'))
+
+
+main()
+
+'''
+Output - 1.54s
+h
+c
+a
+'''
