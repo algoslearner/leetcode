1461a5d25b6d0affbf3db3a7da0ec24ce7936c4e
algoslearner <88410184+algoslearner@users.noreply.github.com>
2022-07-21 13:25:41 -0700

    Update 11_min_cost_to_connect_all_points.py

--- companies/google/leetcode/GRAPH/medium/11_min_cost_to_connect_all_points.py
+++ companies/google/leetcode/GRAPH/medium/11_min_cost_to_connect_all_points.py
@@ -104,6 +104,88 @@ class Solution:
                 if edges_used == n - 1:
                     break
         return mst_cost
+       
+##########################################
+# kruskal's algorithm + heap
+# TC: O(E log E)
+# SC: O(E)
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        if not points or len(points) == 0:
+            return 0
+        size = len(points)
+        pq = []
+        uf = UnionFind(size)
+
+        for i in range(size):
+            x1, y1 = points[i]
+            for j in range(i + 1, size):
+                x2, y2 = points[j]
+                # Calculate the distance between two coordinates.
+                cost = abs(x1 - x2) + abs(y1 - y2)
+                edge = Edge(i, j, cost)
+                pq.append(edge)
+        
+        # Convert pq into a heap.
+        heapq.heapify(pq)
+
+        result = 0
+        count = size - 1
+        while pq and count > 0:
+            edge = heapq.heappop(pq)
+            if not uf.connected(edge.point1, edge.point2):
+                uf.union(edge.point1, edge.point2)
+                result += edge.cost
+                count -= 1
+        return result
+
+class Edge:
+    def __init__(self, point1, point2, cost):
+        self.point1 = point1
+        self.point2 = point2
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+    
+if __name__ == "__main__":
+    points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
+    solution = Solution()
+    print(f"points = {points}")
+    print(f"Minimum Cost to Connect Points = {solution.minCostConnectPoints(points)}")
+ 
+ '''
+ Finished in 59 ms
+points = [[0, 0], [2, 2], [3, 10], [5, 2], [7, 0]]
+Minimum Cost to Connect Points = 20
+'''
 
 ########################################################################################
 # Minimum Spanning Tree - Prim's Algorithm
